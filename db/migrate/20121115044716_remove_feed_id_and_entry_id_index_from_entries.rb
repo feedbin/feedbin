055471327600155ada97c9accee646a8817bcf04
dima <pronix.service@gmail.com>
2013-08-29 11:37:26 +0200

    fixed migration for new installation, remove not used schema.rb, add new indexes for improve db
    Conflicts:
    	db/migrate/20121013082327_add_feed_id_to_entry_states.rb

--- db/migrate/20121115044716_remove_feed_id_and_entry_id_index_from_entries.rb
+++ db/migrate/20121115044716_remove_feed_id_and_entry_id_index_from_entries.rb
@@ -1,6 +1,7 @@
 class RemoveFeedIdAndEntryIdIndexFromEntries < ActiveRecord::Migration
   def up
-    remove_index(:entries, name: :index_entries_on_feed_id_and_entry_id)
+# not exist for new installations
+#    remove_index(:entries, name: :index_entries_on_feed_id_and_entry_id)
   end
 
   def down
