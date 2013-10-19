dc00f9627eab28739aca48abfaddc20fd91573e3
Ben Ubois <ben@benubois.com>
2013-10-18 21:43:43 -0700

    More font choices.

--- config/initializers/fonts.rb
+++ config/initializers/fonts.rb
@@ -1,6 +1,9 @@
 Feedbin::Application.config.font_sizes = (1..10).to_a
 Feedbin::Application.config.fonts = {
     'Helvetica Neue' => 'default',
-    'Sentinel'        => 'serif-1',
-    'Whitney'         => 'sans-serif-1'
+    'Whitney'        => 'sans-serif-1',
+    'Ideal Sans'     => 'sans-serif-2',
+    'Sentinel'       => 'serif-1',
+    'Mercury'        => 'serif-2',
+    'Georgia'        => 'serif-3'
 }
