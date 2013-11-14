b6707e9e4fb1cd48cf8af0dbed6bac37cb110a02
Ben Ubois <ben@benubois.com>
2013-11-14 13:41:55 -0800

    Added a default title for untitled saved searches.

---
+++ app/presenters/saved_search_presenter.rb
@@ -0,0 +1,13 @@
+class SavedSearchPresenter < BasePresenter
+
+  presents :saved_search
+
+  def name
+    if saved_search.name.blank?
+      'Untitled Saved Search'
+    else
+      saved_search.name
+    end
+  end
+
+end
