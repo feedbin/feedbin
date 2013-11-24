c2d09d6bd89302c022c9a43196ed40ec80286814
Ben Ubois <ben@benubois.com>
2013-11-24 10:00:39 -0800

    Separate timing for web and API by host.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,7 +1,13 @@
 ActiveSupport::Notifications.subscribe do |name, start, finish, id, payload|
   if 'process_action.action_controller' == name && payload[:params]['controller'] && payload[:params]['action']
     if finish && start
-      Librato.timing "controller.#{payload[:params]['controller'].gsub('/', '_')}.#{payload[:params]['action']}.time", (finish - start) * 1000
+      time = (finish - start) * 1000
+      Librato.timing "controller.#{payload[:params]['controller'].gsub('/', '_')}.#{payload[:params]['action']}.time", time
+      if payload[:params].present? && payload[:params]['subdomain'].present? && payload[:params]['subdomain'] == 'api'
+        Librato.timing "response_time.api", time, source: Socket.gethostname
+      else
+        Librato.timing "response_time.web", time, source: Socket.gethostname
+      end
     end
     if payload[:db_runtime]
       Librato.timing "controller.#{payload[:params]['controller'].gsub('/', '_')}.#{payload[:params]['action']}.time.db", payload[:db_runtime]
