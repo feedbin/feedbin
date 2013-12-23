64934b5542644c8584872411e342710563cd5f43
Ben Ubois <ben@benubois.com>
2013-12-22 23:05:14 -0800

    Upgraded Feedzirra. For feedbin/support#120.

--- crawler/lib/feedzirra_extension.rb
+++ crawler/lib/feedzirra_extension.rb
@@ -4,8 +4,4 @@ Feedzirra::Feed.add_common_feed_entry_element('_old_public_id_')
 Feedzirra::Feed.add_common_feed_entry_element('_data_')
 Feedzirra::Feed.add_common_feed_entry_element('content')
 
-class Feedzirra::Parser::ITunesRSSItem
-  element :entry_id
-end
-
-ITUNES_RSS_ITEM_FIX_DATE = Time.parse('2013-07-16T15:00:00+00:00')
+Feedzirra::Parser::Atom.preprocess_xml = true
