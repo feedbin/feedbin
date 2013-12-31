b7b535dfac4aa7edbf794765deccfb219666c8ed
Ben Ubois <ben@benubois.com>
2013-12-30 21:49:42 -0800

    Whitelist figure.

--- config/initializers/whitelist.rb
+++ config/initializers/whitelist.rb
@@ -1,2 +1,3 @@
 Feedbin::Application.config.whitelist = HTML::Pipeline::SanitizationFilter::WHITELIST.clone
 Feedbin::Application.config.whitelist[:attributes][:all] += ['id']
+Feedbin::Application.config.whitelist[:elements] += ['figure', 'figcaption']
