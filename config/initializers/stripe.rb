5a00d9d97403780ee8477daf4acd95653663536d
Ben Ubois <ben@benubois.com>
2014-01-03 17:02:38 -0800

    Updated dependencies.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,7 +2,7 @@ Stripe.api_key = ENV['STRIPE_API_KEY']
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 
 StripeEvent.setup do
-  subscribe do |event|
+  all do |event|
     BillingEvent.create(details: event)
   end
 end
