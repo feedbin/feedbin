bc3386a84db92691a697c556e206a4ca5b64c52e
Ben Ubois <ben@benubois.com>
2014-01-21 16:38:21 -0800

    Allow data uris for images.

--- config/initializers/whitelist.rb
+++ config/initializers/whitelist.rb
@@ -1,3 +1,4 @@
 Feedbin::Application.config.whitelist = HTML::Pipeline::SanitizationFilter::WHITELIST.clone
 Feedbin::Application.config.whitelist[:attributes][:all] += ['id']
 Feedbin::Application.config.whitelist[:elements] += ['figure', 'figcaption']
+Feedbin::Application.config.whitelist[:protocols]['img']['src'] += ['data']
