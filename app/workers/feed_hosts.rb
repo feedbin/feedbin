db4bc829608e81225508a58c610dad234dcc9c8a
Ben Ubois <ben@benubois.com>
2014-08-23 20:24:11 -0700

    Accept ids argument.

--- app/workers/feed_hosts.rb
+++ app/workers/feed_hosts.rb
@@ -2,11 +2,13 @@ class FeedHosts
   include Sidekiq::Worker
   sidekiq_options queue: :worker_slow
 
-  def perform(batch)
-    batch_size = 1000
-    start = ((batch - 1) * batch_size) + 1
-    finish = batch * batch_size
-    ids = (start..finish).to_a
+  def perform(batch, ids = nil)
+    if ids.nil?
+      batch_size = 1000
+      start = ((batch - 1) * batch_size) + 1
+      finish = batch * batch_size
+      ids = (start..finish).to_a
+    end
 
     values = []
     feeds = Feed.where(id: ids).pluck(:id, :site_url)
