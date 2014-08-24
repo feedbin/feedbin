035d35a39c88cb8af48af1679ffaabdf8012c460
Ben Ubois <ben@benubois.com>
2014-08-23 21:10:45 -0700

    On demand favicon support.

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -16,11 +16,18 @@ class BasePresenter
     end
   end
 
+  def favicon_with_url(host)
+    @template.content_tag :span, '', class: "favicon-wrap" do
+      @template.content_tag(:span, '', class: "favicon", style: "background-image: url(#{favicon_url(host)});")
+    end
+  end
+
   def favicon_url(host)
     uri = URI::HTTP.build(
       scheme: "https",
-      host: "d34k41xev839cc.cloudfront.net",
-      path: "/#{host}"
+      host: "www.google.com",
+      path: "/s2/favicons",
+      query: {domain: host}.to_query
     )
     uri.scheme = "https"
     uri.to_s
