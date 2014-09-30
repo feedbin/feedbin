5c6d7c41469e8886a178e8e11f686bf788bbdfa9
Ben Ubois <ben@benubois.com>
2014-09-30 06:22:24 -0700

    Upgraded sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
+Sidekiq::Web.app_url = ENV['FEEDBIN_URL']
+
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   web_password = ENV['SIDEKIQ_PASSWORD'] || 'secret'
   username == 'admin' && password == web_password
