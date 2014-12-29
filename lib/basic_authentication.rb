c2b03ecd54adccdc67957e9dfee31002bc707000
Ben Ubois <ben@benubois.com>
2014-12-29 14:08:07 -0800

    Don't modify ApplePushNotifications auth.

--- lib/basic_authentication.rb
+++ lib/basic_authentication.rb
@@ -4,7 +4,7 @@ class BasicAuthentication
   end
 
   def call(env)
-    if env['HTTP_AUTHORIZATION'].respond_to?(:include?) && !env['HTTP_AUTHORIZATION'].include?('Basic')
+    if env['HTTP_AUTHORIZATION'].respond_to?(:include?) && !env['HTTP_AUTHORIZATION'].include?('Basic') && !env['HTTP_AUTHORIZATION'].include?('ApplePushNotifications')
       env['HTTP_AUTHORIZATION'] = "Basic #{env['HTTP_AUTHORIZATION']}"
     end
     @app.call(env)
