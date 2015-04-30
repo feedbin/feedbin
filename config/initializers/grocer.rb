b31181534f3dd31f3bd0f9de993dbd233050836b
Ben Ubois <ben@benubois.com>
2015-04-29 19:42:22 -0700

    New grocer.

--- config/initializers/grocer.rb
+++ config/initializers/grocer.rb
@@ -5,3 +5,12 @@ if ENV['APPLE_PUSH_CERT']
     Grocer.pusher(certificate: certificate)
   end
 end
+
+
+if ENV['APPLE_PUSH_CERT_IOS']
+  p12 = OpenSSL::PKCS12.new(File.read(ENV['APPLE_PUSH_CERT_IOS']))
+  certificate = StringIO.new(p12.certificate.to_pem + p12.key.to_pem)
+  $grocer_ios = ConnectionPool.new(size: 1, timeout: 5) do
+    Grocer.pusher(certificate: certificate)
+  end
+end
