0b637a455cf99fe652a94e1cd6d0ba9cbe700a72
Ben Ubois <ben@benubois.com>
2015-05-18 11:25:21 -0700

    Action migrations.

---
+++ db/migrate/20150425060924_add_tag_ids_and_action_type_to_actions.rb
@@ -0,0 +1,7 @@
+class AddTagIdsAndActionTypeToActions < ActiveRecord::Migration
+  def change
+    add_column :actions, :tag_ids, :integer, array: true, default: []
+    add_column :actions, :action_type, :integer, default: 0
+    add_column :actions, :computed_feed_ids, :integer, array: true, default: []
+  end
+end
