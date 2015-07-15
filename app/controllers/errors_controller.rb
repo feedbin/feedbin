4b2fc3a6ee57765e3f90c2c1bd627afd21fffa9e
Ben Ubois <ben@benubois.com>
2015-07-14 18:30:44 -0700

    404 status.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,7 +3,7 @@ class ErrorsController < ApplicationController
   skip_before_action :authorize
 
   def not_found
-    render layout: 'sub_page'
+    render layout: 'sub_page', status: :not_found
   end
 
   def service_unavailable
