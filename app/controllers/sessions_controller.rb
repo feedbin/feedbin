75205170f551b5d4804a05a7c3aab2815d78d9d3
Ben Ubois <ben@benubois.com>
2015-08-26 17:31:23 -0700

    Redirect for logged in users.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   skip_before_action :authorize
 
   def new
-    @user = current_user
+    redirect_to root_url if signed_in?
   end
 
   def create
