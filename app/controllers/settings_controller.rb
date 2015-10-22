dcf4451c452bd0f33acca81a9e87703569ccab0f
Ben Ubois <ben@benubois.com>
2015-10-21 17:32:05 -0700

    More profile.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -29,8 +29,8 @@ class SettingsController < ApplicationController
 
     start_date = 29.days.ago
     feed_ids = @subscriptions.map {|subscription| subscription.feed_id}
-    entry_counts = Rails.cache.fetch("#{@user.id}:entry_counts:2", expires_in: 24.hours) { get_entry_counts(feed_ids, start_date) }
-    max = Rails.cache.fetch("#{@user.id}:max_entry_count", expires_in: 24.hours) { max_entry_count(feed_ids, start_date) }
+    entry_counts = Rails.cache.fetch("#{@user.id}:entry_counts:2", expires_in: 24.hours) { FeedStat.get_entry_counts(feed_ids, start_date) }
+    max = Rails.cache.fetch("#{@user.id}:max_entry_count", expires_in: 24.hours) { FeedStat.max_entry_count(feed_ids, start_date) }
 
     @subscriptions = @subscriptions.map do |subscription|
       counts = entry_counts[subscription.feed_id]
@@ -242,54 +242,5 @@ class SettingsController < ApplicationController
                                  :hide_recently_read, :hide_updated, :disable_image_proxy)
   end
 
-  def get_entry_counts(feed_ids, start_date)
-    end_date = Time.now
-
-    stats_query = relative_entry_count_query
-
-    entry_counts = {}
-    feed_ids.each do |feed_id|
-      query = ActiveRecord::Base.send(:sanitize_sql_array, [stats_query, start_date, end_date, start_date, feed_id])
-      results = ActiveRecord::Base.connection.execute(query)
-      results.each do |result|
-        if entry_counts.has_key?(feed_id)
-          entry_counts[feed_id] << result['entries_count'].to_i
-        else
-          entry_counts[feed_id] = [result['entries_count'].to_i]
-        end
-      end
-    end
-    entry_counts
-  end
-
-  def max_entry_count(feed_ids, start_date)
-    max_query = "SELECT COALESCE(MAX(entries_count), 0) as max FROM feed_stats WHERE feed_id IN(?) and day >= ?"
-    max_query = ActiveRecord::Base.send(:sanitize_sql_array, [max_query, feed_ids, start_date])
-    max = ActiveRecord::Base.connection.execute(max_query)
-    max.first['max'].to_i
-  end
-
-  def relative_entry_count_query
-    <<-eos
-      SELECT
-        date,
-        coalesce(entries_count,0) AS entries_count
-      FROM
-      generate_series(
-        ?::date,
-        ?::date,
-        '1 day'
-      ) AS date
-      LEFT OUTER JOIN (
-        SELECT
-        day,
-        entries_count
-        FROM feed_stats
-        WHERE day >= ?
-        AND feed_id = ?
-      ) results
-      ON (date = results.day)
-    eos
-  end
 
 end
--- app/presenters/subscription_presenter.rb
+++ app/presenters/subscription_presenter.rb
@@ -2,4 +2,48 @@ class SubscriptionPresenter < BasePresenter
 
   presents :subscription
 
+  def graph_volume
+    counts.sum
+  end
+
+  def graph_date_start
+    days.ago.to_s(:day_month).upcase
+  end
+
+  def graph_date_mid
+    total_days = (Time.now - days.ago).to_i
+    (days.ago + total_days / 2).to_s(:day_month).upcase
+  end
+
+  def graph_date_end
+    Time.now.to_s(:day_month).upcase
+  end
+
+  def graph_bars
+    max = counts.max
+    output = ""
+    counts.map { |count| (count.to_f / max.to_f) * 100 }.each do |percentage|
+      output += @template.content_tag(:div, '', style: "height: #{percentage}%")
+    end
+    output.html_safe
+  end
+
+  def graph_max
+    counts.max
+  end
+
+  def graph_mid
+    counts.max / 2
+  end
+
+  private
+
+  def counts
+    @counts ||= FeedStat.get_entry_counts([subscription.feed.id], days.ago).values.first
+  end
+
+  def days
+    29.days
+  end
+
 end
