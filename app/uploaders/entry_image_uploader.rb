ab5b07032af2ca1c13d088299ccb20f97e2a65de
Ben Ubois <ben@benubois.com>
2015-10-27 15:45:04 -0700

    Long term caching for images.

--- app/uploaders/entry_image_uploader.rb
+++ app/uploaders/entry_image_uploader.rb
@@ -6,4 +6,10 @@ class EntryImageUploader < CarrierWave::Uploader::Base
     "public-images/#{Time.now.utc.strftime("%F")}"
   end
 
+  def fog_attributes
+    {
+      'Cache-Control' => 'max-age=315360000, public'
+    }
+  end
+
 end
