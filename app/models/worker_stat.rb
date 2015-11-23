ee9bd3346036d7e7e69f9e08c3ecc8bb22092e79
Ben Ubois <ben@benubois.com>
2015-11-22 19:43:14 -0800

    Track version.

--- app/models/worker_stat.rb
+++ app/models/worker_stat.rb
@@ -1,6 +1,6 @@
 class WorkerStat
   def call(worker, item, queue)
-    Librato.timing "worker.#{worker.class.to_s.underscore}" do
+    Librato.timing "worker.#{worker.class.to_s.underscore}", source: Sidekiq::VERSION do
       yield
     end
   end
