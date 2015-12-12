96ebcae97a6943ccf5c43a72d827ab9c6afd00ce
Ben Ubois <ben@benubois.com>
2015-12-12 14:36:33 -0800

    New feed refresher

---
+++ app/workers/feed_refresher.rb
@@ -0,0 +1,67 @@
+require_relative '../../lib/batch_jobs'
+
+class FeedRefresher
+  include BatchJobs
+  include Sidekiq::Worker
+
+  def perform(batch, priority_refresh)
+    feed_ids = build_ids(batch)
+    count = priority_refresh ? 1 : 0
+    feeds = Feed.where(id: feed_ids).where("subscriptions_count > ?", count).select(:id, :feed_url, :etag, :last_modified, :subscriptions_count, :push_expiration)
+    subscriptions = Subscription.where(feed_id: feed_ids, active: true, muted: false).group(:feed_id).count
+
+    arguments = feeds.each_with_object([]) do |feed, array|
+      if subscriptions.has_key?(feed.id)
+        array << Arguments.new(feed, url_template).to_a
+      end
+    end
+
+    Sidekiq::Client.push_bulk(
+      'args'  => arguments,
+      'class' => 'FeedRefresherFetcher',
+      'queue' => 'feed_refresher_fetcher',
+      'retry' => false
+    )
+  end
+
+  def url_template
+    @url_template ||= begin
+      template = nil
+      if ENV['PUSH_URL']
+        uri = URI(ENV['PUSH_URL'])
+        id = 454545
+        template = Rails.application.routes.url_helpers.push_feed_url(Feed.new(id: id), protocol: uri.scheme, host: uri.host)
+        template = template.sub(id.to_s, "%d")
+      end
+      template
+    end
+  end
+
+  class Arguments
+    def initialize(feed, push_url)
+      @feed = feed
+      @push_url = push_url
+      @body = nil # only needed when receiving PuSH
+    end
+
+    def to_a
+      [@feed.id, @feed.feed_url, @feed.etag, @feed.last_modified, @feed.subscriptions_count, @body, push_callback, hub_secret]
+    end
+
+    private
+
+    def push_callback
+      (push?) ? @push_url % @feed.id : nil
+    end
+
+    def hub_secret
+      (push?) ? Push::hub_secret(@feed.id) : nil
+    end
+
+    def push?
+      @push ||= @push_url && @feed.push_expiration.nil? || @feed.push_expiration < Time.now
+    end
+
+  end
+
+end
--- lib/batch_jobs.rb
+++ lib/batch_jobs.rb
@@ -1,15 +1,15 @@
 module BatchJobs
-  BATCH_SIZE = 1_000
-  
-  def job_args(total_records)
+  BATCH_SIZE = 2
+
+  def job_args(total_records, *args)
     batch_count = (total_records.to_f/BATCH_SIZE.to_f).ceil
     jobs = []
     1.upto(batch_count) do |batch|
-      jobs.push([batch])
+      jobs.push([batch].concat(args))
     end
     jobs
   end
-  
+
   def build_ids(batch)
     start = ((batch - 1) * BATCH_SIZE) + 1
     finish = batch * BATCH_SIZE
