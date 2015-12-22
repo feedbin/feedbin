bdebb7ee85641f2d035922df719aaf4de7bf1226
Ben Ubois <ben@benubois.com>
2015-12-22 14:58:36 -0800

    Record hostname

--- app/models/worker_stat.rb
+++ app/models/worker_stat.rb
@@ -1,9 +1,9 @@
 class WorkerStat
   def call(worker, item, queue)
     title = "worker.#{worker.class.to_s.underscore}"
-    Librato.increment "#{title}.count"
-    Librato.increment "worker_perform"
-    Librato.timing title do
+    Librato.increment "#{title}.count", source: Socket.gethostname
+    Librato.increment "worker_perform", source: Socket.gethostname
+    Librato.timing title, source: Socket.gethostname do
       yield
     end
   end
