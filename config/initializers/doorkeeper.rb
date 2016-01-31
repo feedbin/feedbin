4e818d24ae505f4c29819c4b3907e3a9947c746a
Max Meyers <maxmeyers@me.com>
2016-01-31 12:12:00 -0800

    Authenticate the user as part of the OAuth flow.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,12 +1,13 @@
+include SessionsHelper
+
 Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    authorize
+    current_user
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
