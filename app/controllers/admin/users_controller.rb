474fa08375e930b3ffe8ab021c01758310908b2e
Ben Ubois <ben@benubois.com>
2016-02-16 16:22:53 -0800

    Added user search

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,11 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = User.page(params[:page])
+
+    if params.has_key?(:q)
+      @users = User.page(params[:page]).where("email LIKE :query", query: "%#{params[:q]}%")  + DeletedUser.page(params[:page]).where("email LIKE :query", query: "%#{params[:q]}%")
+    else
+      @users = User.page(params[:page]) + DeletedUser.page(params[:page])
+    end
     render layout: 'settings'
   end
 
