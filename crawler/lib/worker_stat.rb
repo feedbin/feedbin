1343b8b9f132f8179852eb67f96b25ef571e7414
Ben Ubois <ben@benubois.com>
2016-06-01 19:24:36 -0700

    Removed missing method.

--- crawler/lib/worker_stat.rb
+++ crawler/lib/worker_stat.rb
@@ -1,6 +1,6 @@
 class WorkerStat
   def call(worker, item, queue)
-    title = "worker.#{worker.class.to_s.underscore}"
+    title = "worker.#{worker.class.to_s}"
     Librato.increment "#{title}.count", source: Socket.gethostname
     Librato.timing title, source: Socket.gethostname do
       yield
