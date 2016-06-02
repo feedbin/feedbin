3b21638a3038e5bb06b947521f63a047a3c2af83
Ben Ubois <ben@benubois.com>
2016-06-01 19:22:03 -0700

    Added worker stats.

---
+++ crawler/lib/worker_stat.rb
@@ -0,0 +1,9 @@
+class WorkerStat
+  def call(worker, item, queue)
+    title = "worker.#{worker.class.to_s.underscore}"
+    Librato.increment "#{title}.count", source: Socket.gethostname
+    Librato.timing title, source: Socket.gethostname do
+      yield
+    end
+  end
+end
