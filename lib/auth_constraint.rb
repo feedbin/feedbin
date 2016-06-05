ff9b743a4be406413aba758cf2e8c611ea286e87
Ben Ubois <ben@benubois.com>
2016-06-04 23:52:29 -0700

    Try moving sidekiq back to routes.

---
+++ lib/auth_constraint.rb
@@ -0,0 +1,11 @@
+require_relative '../config/boot'
+require_relative '../config/environment'
+
+class AuthConstraint
+  def self.admin?(request)
+    auth_token = request.cookie_jar.signed[:auth_token]
+    return false unless auth_token
+    user = User.find_by_auth_token(auth_token)
+    user && user.admin?
+  end
+end
