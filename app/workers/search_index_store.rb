95215dc6e14e7100c159563d6eadedb4c239b3bb
Ben Ubois <ben@benubois.com>
2016-06-12 18:15:10 -0700

    Search index updates

--- app/workers/search_index_store.rb
+++ app/workers/search_index_store.rb
@@ -6,24 +6,27 @@ class SearchIndexStore
     klass = klass.constantize
     record = klass.find(id)
     record.__elasticsearch__.index_document
-    if !update
-      result = klass.__elasticsearch__.client.percolate(
-        index: klass.index_name,
-        type: klass.document_type,
-        percolate_format: 'ids',
-        body: {
-          doc: record.as_indexed_json,
-          filter: {
-            term: { feed_id: record.feed_id }
-          }
-        }
-      )
-      percolator_ids = result['matches'].map(&:to_i)
-      if percolator_ids.present?
-        ActionsPerform.perform_async(id, percolator_ids)
-      end
-    end
+    percolate(record, klass) if !update
   rescue ActiveRecord::RecordNotFound
+  end
+
+  def percolate(record, klass)
+    result = klass.__elasticsearch__.client.percolate(
+      index: klass.index_name,
+      type: klass.document_type,
+      percolate_format: 'ids',
+      body: {
+        doc: record.as_indexed_json,
+        filter: {
+          term: { feed_id: record.feed_id }
+        }
+      }
+    )
+
+    ids = result['matches'].map(&:to_i)
+    if ids.present?
+      ActionsPerform.perform_async(record.id, ids)
+    end
   end
 
 end
