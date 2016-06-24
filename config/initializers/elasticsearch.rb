74195e79e3e5662204b23d412c7249c3c12bf7a0
Ben Ubois <ben@benubois.com>
2016-06-24 16:24:34 -0700

    Skip verify.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,10 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new log: Rails.env.development?
+options = {
+  log: Rails.env.development?,
+  transport_options: {
+    ssl: { verify: false }
+  }
+}
+Elasticsearch::Model.client = Elasticsearch::Client.new(options)
 if Rails.env.development?
   Elasticsearch::Model.client.transport.tracer = ActiveSupport::Logger.new('log/elasticsearch.log')
   Entry.__elasticsearch__.create_index! rescue nil
