d905cc44f7837d1c7811b16ddc72e69a3ff6c52f
Ben Ubois <ben@benubois.com>
2016-07-22 14:06:24 -0700

    Generate certificate.

--- test/controllers/apple_push_notifications_controller_test.rb
+++ test/controllers/apple_push_notifications_controller_test.rb
@@ -5,6 +5,7 @@ class ApplePushNotificationsControllerTest < ActionController::TestCase
   def setup
     @user = users(:ben)
     @token = ActionsController.new().send(:authentication_token, @user)
+    ENV['APPLE_PUSH_CERT'] = create_cert("/tmp/p12.p12")
   end
 
   test "should create push package" do
@@ -45,6 +46,18 @@ class ApplePushNotificationsControllerTest < ActionController::TestCase
 
   def set_authorization_header
     @request.headers["Authorization"] = "ApplePushNotifications #{@token}"
+  end
+
+  def create_cert(path)
+    rsa_key = OpenSSL::PKey::RSA.new(2048)
+    cert = OpenSSL::X509::Certificate.new()
+    cert.not_before = Time.new()
+    cert.not_after = cert.not_before + (60*60*24*365)
+    cert.public_key = rsa_key.public_key
+    cert.sign rsa_key, OpenSSL::Digest::SHA1.new()
+    p12 = OpenSSL::PKCS12.create(nil, nil, rsa_key, cert)
+    File.open(path, 'wb') { |file| file.write p12.to_der }
+    path
   end
 
 end
