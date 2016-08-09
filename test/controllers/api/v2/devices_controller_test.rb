2170eea8be3949a2bc7bc35d49e625527df904c8
Ben Ubois <ben@benubois.com>
2016-08-09 11:37:13 -0700

    Device tests

---
+++ test/controllers/api/v2/devices_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class Api::V2::DevicesControllerTest < ApiControllerTestCase
+
+  setup do
+    @user = users(:ben)
+  end
+
+  test "should create device" do
+    @request.headers["Content-Type"] = "application/json; charset=utf-8"
+    login_as @user
+
+    assert_difference "Device.count", +1 do
+      post :create, device: {token: 'token', device_type: Device.device_types[:ios], model: 'model', application: 'application', operating_system: 'operating_system'}, format: :json
+      assert_response :success
+    end
+  end
+
+  test "should get ios_test" do
+    Sidekiq::Worker.clear_all
+    login_as @user
+    @user.feeds.first.entries.create(public_id: 'new')
+    assert_difference "DevicePushNotificationSend.jobs.size", +1 do
+      get :ios_test, format: :json
+      assert_response :success
+    end
+  end
+
+  test "should get safari_test" do
+    Sidekiq::Worker.clear_all
+    login_as @user
+    @user.feeds.first.entries.create(public_id: 'new')
+    assert_difference "SafariPushNotificationSend.jobs.size", +1 do
+      get :safari_test, format: :json
+      assert_response :success
+    end
+  end
+
+end
