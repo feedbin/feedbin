e1829882ceb381f74891b6d3a96525fd2496f394
Ben Ubois <ben@benubois.com>
2016-08-09 15:30:52 -0700

    recently_read_entries tests.

---
+++ test/controllers/api/v2/recently_read_entries_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class Api::V2::RecentlyReadEntriesControllerTest < ApiControllerTestCase
+
+  setup do
+    @user = users(:new)
+    @feeds = create_feeds(@user)
+    @entries = @user.entries
+  end
+
+  test "should get index" do
+    recently_read = @entries.map do |entry|
+      RecentlyReadEntry.create(user: @user, entry: entry)
+    end
+
+    login_as @user
+    get :index, format: :json
+    assert_response :success
+    response = parse_json
+    assert_equal(Set.new(@entries.map(&:id)), Set.new(response))
+  end
+
+  test "should create recently read entry" do
+    @request.headers["Content-Type"] = "application/json; charset=utf-8"
+    entry = @entries.sample
+
+    login_as @user
+    assert_difference "RecentlyReadEntry.count", +1 do
+      post :create, id: entry, recently_read_entries: [entry], format: :json
+      assert_response :success
+    end
+  end
+
+end
