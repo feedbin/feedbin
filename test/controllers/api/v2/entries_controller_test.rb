7e35c9d7838a77eb4770c068b82d44d43c636c56
Ben Ubois <ben@benubois.com>
2016-08-09 16:18:06 -0700

    Debug failing test.

--- test/controllers/api/v2/entries_controller_test.rb
+++ test/controllers/api/v2/entries_controller_test.rb
@@ -8,71 +8,80 @@ class Api::V2::EntriesControllerTest < ApiControllerTestCase
     @entries = @user.entries
   end
 
-  test "should get index" do
-    login_as @user
-    get :index, format: :json
-    assert_response :success
-    assert_equal @entries.length, assigns(:entries).length
-  end
-
-  test "should show entry" do
-    login_as @user
-    entry = @entries.first
-
-    get :show, id: entry, format: :json
-    assert_response :success
-
-    result = parse_json
-    assert_has_keys(entry_keys, result)
-  end
-
-  test "should show entry with all keys" do
-    login_as @user
-    entry = @entries.first
-
-    get :show, id: entry, format: :json, include_content_diff: 'true', include_enclosure: 'true', include_original: 'true'
-    assert_response :success
-
-    result = parse_json
-    assert_has_keys(entry_keys(true), result)
-  end
-
-  test "should get text format" do
-    login_as @user
-    get :text, id: @entries.first, format: :json
-    assert_response :success
-  end
-
   test "should get specific ids" do
     login_as @user
     entries = @entries.sample(2)
-    get :index, format: :json, ids: entries.map(&:id).join(',')
+    ids = entries.map(&:id).join(',')
+    get :index, format: :json, ids: ids
+
+    allowed_feed_ids = []
+    allowed_feed_ids = allowed_feed_ids.concat(@user.starred_entries.select('DISTINCT feed_id').map {|entry| entry.feed_id})
+    allowed_feed_ids = allowed_feed_ids.concat(@user.subscriptions.pluck(:feed_id))
+
+    puts "allowed: #{allowed_feed_ids}"
+    puts "actual: #{entries.map(&:feed_id)}"
+    puts "id request: #{ids}"
     assert_response :success
     assert_equal_ids(entries, parse_json)
   end
 
-  test "should get starred entries" do
-    login_as @user
-    entries = @entries.sample(2)
-
-    entries.each do |entry|
-      StarredEntry.create_from_owners(@user, entry)
-    end
-
-    get :index, format: :json, starred: 'true'
-    assert_response :success
-    assert_equal_ids(entries, parse_json)
-  end
-
-  test "should get entries since date" do
-    login_as @user
-    entry = @entries.sample
-    date = entry.created_at.iso8601(6)
-    get :index, format: :json, since: date
-
-    expected = Entry.where("created_at > :time", {time: entry.created_at})
-    assert_equal_ids expected, parse_json
-  end
+  # test "should get index" do
+  #   login_as @user
+  #   get :index, format: :json
+  #   assert_response :success
+  #   assert_equal @entries.length, assigns(:entries).length
+  # end
+  #
+  # test "should show entry" do
+  #   login_as @user
+  #   entry = @entries.first
+  #
+  #   get :show, id: entry, format: :json
+  #   assert_response :success
+  #
+  #   result = parse_json
+  #   assert_has_keys(entry_keys, result)
+  # end
+  #
+  # test "should show entry with all keys" do
+  #   login_as @user
+  #   entry = @entries.first
+  #
+  #   get :show, id: entry, format: :json, include_content_diff: 'true', include_enclosure: 'true', include_original: 'true'
+  #   assert_response :success
+  #
+  #   result = parse_json
+  #   assert_has_keys(entry_keys(true), result)
+  # end
+  #
+  # test "should get text format" do
+  #   login_as @user
+  #   get :text, id: @entries.first, format: :json
+  #   assert_response :success
+  # end
+  #
+  # test "should get starred entries" do
+  #   login_as @user
+  #   entries = @entries.sample(2)
+  #
+  #   entries.each do |entry|
+  #     StarredEntry.create_from_owners(@user, entry)
+  #   end
+  #
+  #   get :index, format: :json, starred: 'true'
+  #   assert_response :success
+  #   assert_equal_ids(entries, parse_json)
+  # end
+  #
+  # test "should get entries since date" do
+  #   login_as @user
+  #   entry = @entries.sample
+  #   date = entry.created_at.iso8601(6)
+  #   get :index, format: :json, since: date
+  #
+  #   expected = Entry.where("created_at > :time", {time: entry.created_at})
+  #   assert_equal_ids expected, parse_json
+  # end
 
   private
 
