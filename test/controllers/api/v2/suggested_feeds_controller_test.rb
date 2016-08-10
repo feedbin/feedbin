49c5ccf35ff98fb832063bb7db0ee4af54d7132b
Ben Ubois <ben@benubois.com>
2016-08-09 19:11:19 -0700

    API tests.

---
+++ test/controllers/api/v2/suggested_feeds_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class Api::V2::SuggestedFeedsControllerTest < ApiControllerTestCase
+
+  setup do
+    @user = users(:ben)
+    @category = SuggestedCategory.create!(name: "Popular")
+    @feed = Feed.create(feed_url: Faker::Internet.url)
+    @suggested_feed = SuggestedFeed.create!(suggested_category: @category, feed: @feed)
+  end
+
+  test "gets index" do
+    login_as @user
+    get :index, format: :json
+    assert_response :success
+    results = parse_json
+    assert_has_keys keys, results.first
+  end
+
+  test "should subscribe to suggested feed" do
+    login_as @user
+
+    assert_difference "Subscription.count", +1 do
+      post :subscribe, id: @suggested_feed, format: :json
+      assert_response :success
+    end
+  end
+
+  test "should unsubscribe from suggested feed" do
+    @user.subscriptions.create(feed: @feed)
+    login_as @user
+
+    assert_difference "Subscription.count", -1 do
+      delete :unsubscribe, id: @suggested_feed, format: :json
+      assert_response :success
+    end
+  end
+
+  private
+
+  def keys
+    %w[id feed_id suggested_category_id title host]
+  end
+
+
+end
---
+++ test/controllers/api/v2/tags_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class Api::V2::TagsControllerTest < ApiControllerTestCase
+
+  setup do
+    @user = users(:ben)
+    @tagging = @user.feeds.first.tag('new tag', @user, false).first
+  end
+
+  test "should get index" do
+    login_as @user
+    get :index, format: :json
+    assert_response :success
+    results = parse_json
+    assert_has_keys keys, results.first
+  end
+
+  private
+
+  def keys
+    %w[id name]
+  end
+
+end
