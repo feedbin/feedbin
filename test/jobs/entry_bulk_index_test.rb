7c3afc444db68725d644b327ae87125ae721a7aa
Ben Ubois <ben@benubois.com>
2016-08-24 21:15:31 -0700

    Job test

---
+++ test/jobs/entry_bulk_index_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class EntryBulkIndexTest < ActiveSupport::TestCase
+  test "should bulk index entries in elasticsearch" do
+    Sidekiq::Queues["worker_slow"].clear
+    feed = Feed.first
+    entries = SecureRandom.random_number(10).times.map do
+      feed.entries.create(
+        content: Faker::Lorem.paragraph,
+        public_id: SecureRandom.hex,
+      )
+    end
+
+    Sidekiq::Testing.inline! do
+      EntryBulkIndex.new().perform(nil, true, Entry.last.id)
+    end
+    Entry.__elasticsearch__.refresh_index!
+
+    query = {
+      query: {
+        filtered: {
+          filter: {
+            terms: { id: entries.map(&:id) }
+          }
+        }
+      }
+    }
+    assert_equal entries.count, Entry.search(query).results.total
+  end
+end
