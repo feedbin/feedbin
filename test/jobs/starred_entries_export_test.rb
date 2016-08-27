da7bf591e9dd3eaa836c852f73ebd9790b088fb3
Ben Ubois <ben@benubois.com>
2016-08-26 20:58:25 -0700

    Job tests.

---
+++ test/jobs/starred_entries_export_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class StarredEntriesExportTest < ActiveSupport::TestCase
+  test "should send email" do
+    user = users(:ben)
+    entry = create_entry(user.feeds.first)
+    StarredEntry.create_from_owners(user, entry)
+
+    assert_difference "ActionMailer::Base.deliveries.count", +1 do
+      StarredEntriesExport.new().tap do |job|
+        def job.upload_file(file)
+          JSON.parse(File.open(file).read)
+          Faker::Internet.url
+        end
+        job.perform(user.id)
+      end
+    end
+
+  end
+end
---
+++ test/jobs/trial_expiration_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class TrialExpirationTest < ActiveSupport::TestCase
+  test "should deactivate users" do
+    StripeMock.start
+    create_stripe_plan(plans(:trial))
+    user = users(:ann)
+    user.created_at = Feedbin::Application.config.trial_days.days.ago
+    user.save(validate: false)
+
+    assert_not user.suspended
+    TrialExpiration.new().perform
+    assert user.reload.suspended
+    StripeMock.stop
+  end
+end
