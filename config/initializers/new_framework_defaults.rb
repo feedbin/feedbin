3a100b5fb919a1c2b1d967a3111333e7e980b6a7
Ben Ubois <ben@benubois.com>
2016-09-20 00:50:12 -0700

    Misc fixes.

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -20,4 +20,4 @@ ActiveSupport.to_time_preserves_timezone = false
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
-ActiveSupport.halt_callback_chains_on_return_false = true
+ActiveSupport.halt_callback_chains_on_return_false = false
--- lib/tasks/kill_postgres_connections.rake
+++
@@ -1,8 +0,0 @@
-task :kill_postgres_connections => :environment do
-  unless ENV['HEROKU_APP']
-    db_name = "#{File.basename(Rails.root)}_#{Rails.env}"
-    sh = "ps xa | grep postgres: | grep #{db_name} | grep -v grep | awk '{print $1}' | xargs kill"
-    puts `#{sh}`
-  end
-end
-task "db:drop" => :kill_postgres_connections
