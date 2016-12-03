a9f2751a8f4bca802df22ffc135f2c59db01def7
Ben Ubois <ben@benubois.com>
2016-12-03 03:27:48 -0800

    Added norender for Diffbot speed.

--- app/models/diffbot_parser.rb
+++ app/models/diffbot_parser.rb
@@ -47,7 +47,12 @@ class DiffbotParser
   def result
     @result ||= begin
       uri = URI.parse(BASE_URL)
-      uri.query = { 'url' => url, discussion: false, 'token' => ENV['DIFFBOT_TOKEN'] }.to_query
+      uri.query = {
+        url: url,
+        discussion: false,
+        norender: "norender",
+        token: ENV['DIFFBOT_TOKEN']
+      }.to_query
       response = HTTP.timeout(:global, write: 10, connect: 10, read: 10)
       if body
         response = response.headers(content_type: "text/html; charset=utf-8").post(uri, body: body)
