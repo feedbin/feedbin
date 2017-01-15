22f21c684888d2dc8c636643f72e7f713004255d
Ben Ubois <ben@benubois.com>
2017-01-14 22:29:14 -0800

    Remove repeated whitespace.

--- app/models/content_formatter.rb
+++ app/models/content_formatter.rb
@@ -102,7 +102,7 @@ class ContentFormatter
     sanitize_config = Sanitize::Config::BASIC.dup
     sanitize_config = sanitize_config.merge(remove_contents: ['script', 'style', 'iframe', 'object', 'embed', 'figure'])
     content = Sanitize.fragment(content, sanitize_config)
-    content = content.squeeze.strip
+    content = content.squeeze(" \t\n").strip
     ApplicationController.helpers.sanitize(content, tags: []).truncate(86, :separator => " ").squish
   rescue
     ''
