d0319e7662157d8392856f3575ff410566198c15
Ben Ubois <ben@benubois.com>
2017-06-27 15:29:09 -0700

    Setup action on touch.

--- app/models/action.rb
+++ app/models/action.rb
@@ -15,6 +15,7 @@ class Action < ApplicationRecord
   before_validation :compute_feed_ids
   before_save :percolate_setup
   after_destroy :percolate_remove
+  after_touch :percolate_setup
 
   def percolate_setup
     percolator_query = self.query
