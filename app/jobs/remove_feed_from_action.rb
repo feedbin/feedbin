d1c0e9e84378e83a173f600470feecac0b34d177
Ben Ubois <ben@benubois.com>
2017-08-10 17:18:16 -0700

    Modify actions in the background.

---
+++ app/jobs/remove_feed_from_action.rb
@@ -0,0 +1,16 @@
+class RemoveFeedFromAction
+  include Sidekiq::Worker
+  sidekiq_options queue: :critical
+
+  def perform(user_id, feed_id)
+    user = User.find(user_id)
+    actions = user.actions
+    actions.each do |action|
+      feed_ids = action.feed_ids || []
+      action.feed_ids = feed_ids - [feed_id.to_s]
+      action.automatic_modification = true
+      action.save!
+    end
+  end
+
+end
