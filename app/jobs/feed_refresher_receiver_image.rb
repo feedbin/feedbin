4774d8adc326030ec04d1604ad93e6619129f612
Ben Ubois <ben@benubois.com>
2017-08-17 15:25:12 -0700

    Skip missing entries.

--- app/jobs/feed_refresher_receiver_image.rb
+++ app/jobs/feed_refresher_receiver_image.rb
@@ -3,13 +3,14 @@ class FeedRefresherReceiverImage
   sidekiq_options queue: :feed_refresher_receiver
 
   def perform(public_id, url)
-    entry = Entry.find_by_public_id(public_id)
+    entry = Entry.find_by_public_id!(public_id)
     if entry.published > 4.weeks.ago && entry.data['itunes_image_processed'].nil?
       data = entry.data || {}
       data['itunes_image'] = url
       entry.update(data: data)
       ItunesImage.perform_async(entry.id, entry.data['itunes_image'])
     end
+  rescue ActiveRecord::RecordNotFound
   end
 
 end
