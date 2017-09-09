fe5dafc103412dfea065f64edb2f49c201bc7a34
Ben Ubois <ben@benubois.com>
2017-09-09 05:59:59 -0700

    Turn off subdomains HSTS.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = { hsts: { subdomains: false } }
 
   # Less verbose logs
   config.lograge.enabled = true
