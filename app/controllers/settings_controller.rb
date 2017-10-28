9c099920b8f6a6ff125592707937d8db592872b4
Ben Ubois <ben@benubois.com>
2017-10-28 17:46:48 -0400

    Billing updates.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -48,7 +48,7 @@ class SettingsController < ApplicationController
   def billing
     @user = current_user
 
-    @default_plan = Plan.where(price_tier: @user.price_tier, stripe_id: ['basic-yearly-2', 'basic-yearly-3']).first
+    @default_plan = Plan.where(price_tier: @user.price_tier, stripe_id: ['basic-yearly', 'basic-yearly-2', 'basic-yearly-3']).first
 
     @next_payment = @user.billing_events.where(event_type: 'invoice.payment_succeeded')
     @next_payment = @next_payment.to_a.sort_by {|next_payment| -next_payment.event_object["date"] }
