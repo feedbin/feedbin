e160f8b307edcbc864f422dcabff0d0902d780b7
Ben Ubois <ben@benubois.com>
2017-12-28 19:49:12 -0800

    Updates.

--- app/models/source/twitter_data.rb
+++ app/models/source/twitter_data.rb
@@ -3,7 +3,7 @@ class Source::TwitterData < Source
   def call
     twitter_url = Feedkit::TwitterURLRecognizer.new(@url, @config[:twitter_screen_name])
     if twitter_url.valid?
-      twitter = Feedkit::TwitterFeed.new(twitter_url, @config[:twitter_access_token], @config[:twitter_access_secret])
+      twitter = Feedkit::Tweets.new(twitter_url, @config[:twitter_access_token], @config[:twitter_access_secret])
       feed = Feed.where(feed_url: twitter.url.to_s).take
       if !feed
         feed = Feed.create_from_parsed_feed(twitter.feed)
