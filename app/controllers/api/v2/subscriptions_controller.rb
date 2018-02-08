d771928a1a2aa0a4b24c9870993e992128731ada
Ben Ubois <ben@benubois.com>
2018-02-08 13:47:18 -0800

    Add head support.

--- app/controllers/api/v2/subscriptions_controller.rb
+++ app/controllers/api/v2/subscriptions_controller.rb
@@ -8,29 +8,33 @@ module Api
       before_action :validate_create, only: [:create]
 
       def index
-        @user = current_user
-        @user.update(api_client: request.user_agent)
-        respond_to do |format|
-          format.json do
-            @subscriptions = @user.subscriptions.includes(:feed).order("subscriptions.created_at DESC")
-            if params.has_key?(:since)
-              time = Time.iso8601(params[:since])
-              @subscriptions = @subscriptions.where("subscriptions.created_at > :time", {time: time})
+        if request.head?
+          render status: 200
+        else
+          @user = current_user
+          @user.update(api_client: request.user_agent)
+          respond_to do |format|
+            format.json do
+              @subscriptions = @user.subscriptions.includes(:feed).order("subscriptions.created_at DESC")
+              if params.has_key?(:since)
+                time = Time.iso8601(params[:since])
+                @subscriptions = @subscriptions.where("subscriptions.created_at > :time", {time: time})
+              end
+              if @subscriptions.any?
+                fresh_when(etag: @subscriptions, last_modified: @subscriptions.maximum(:updated_at))
+              else
+                @subscriptions = []
+              end
             end
-            if @subscriptions.any?
-              fresh_when(etag: @subscriptions, last_modified: @subscriptions.maximum(:updated_at))
-            else
-              @subscriptions = []
+            format.xml do
+              @tags = @user.feed_tags
+              @feeds = @user.feeds
+              @titles = {}
+              @user.subscriptions.pluck(:feed_id, :title).each do |feed_id, title|
+                @titles[feed_id] = title
+              end
+              render template: 'subscriptions/index'
             end
-          end
-          format.xml do
-            @tags = @user.feed_tags
-            @feeds = @user.feeds
-            @titles = {}
-            @user.subscriptions.pluck(:feed_id, :title).each do |feed_id, title|
-              @titles[feed_id] = title
-            end
-            render template: 'subscriptions/index'
           end
         end
       end
