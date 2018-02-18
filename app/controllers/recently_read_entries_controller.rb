212cc1a43a87785cb9a56deb80f1800b2902fa15
Ben Ubois <ben@benubois.com>
2018-02-18 10:08:43 -0800

    Load less stuff for recently read.

--- app/controllers/recently_read_entries_controller.rb
+++ app/controllers/recently_read_entries_controller.rb
@@ -4,11 +4,9 @@ class RecentlyReadEntriesController < ApplicationController
     @user = current_user
     update_selected_feed!("collection_recently_read")
 
-    recently_read_entries = @user.recently_read_entries.order(id: :desc).limit(100)
-    recently_read_entry_ids = []
-    recently_read_entries.each {|recently_read_entry| recently_read_entry_ids << recently_read_entry.entry_id}
+    recently_read_entry_ids = @user.recently_read_entries.order(id: :desc).limit(100).pluck(:entry_id)
     @entries = Entry.where(id: recently_read_entry_ids).includes(feed: [:favicon]).entries_list
-    @entries = @entries.sort_by{ |entry| recently_read_entry_ids.index(entry.id) }
+    @entries = @entries.sort_by { |entry| recently_read_entry_ids.index(entry.id) }
 
     @type = 'recently_read'
     @collection_title = 'Recently Read'
