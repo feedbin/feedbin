d0dd76b7207725d99c8d331cdbe093871c412a52
Igor Zubkov <igor.zubkov@gmail.com>
2018-06-08 13:02:12 +0300

    Fix migration

--- db/migrate/20121013082327_add_feed_id_to_entry_states.rb
+++ db/migrate/20121013082327_add_feed_id_to_entry_states.rb
@@ -3,10 +3,10 @@ class AddFeedIdToEntryStates < ActiveRecord::Migration[4.2]
     add_column :entry_states, :feed_id, :integer
     add_index :entry_states, :feed_id
 
-    EntryState.find_each do |entry_state|
-      entry_state.feed_id = entry_state.entry.feed_id
-      entry_state.save!
-    end
+    # EntryState.find_each do |entry_state|
+    #   entry_state.feed_id = entry_state.entry.feed_id
+    #   entry_state.save!
+    # end
   end
 
   def down
