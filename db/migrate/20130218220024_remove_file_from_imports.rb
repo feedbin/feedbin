5ebe643c4c8b4e707ac1e11f7344b57d3b41d07a
Igor Zubkov <igor.zubkov@gmail.com>
2018-06-08 20:51:39 +0300

    Fix migrations

--- db/migrate/20130218220024_remove_file_from_imports.rb
+++ db/migrate/20130218220024_remove_file_from_imports.rb
@@ -1,11 +1,4 @@
 class RemoveFileFromImports < ActiveRecord::Migration[4.2]
-  def self.up
-    drop_attached_file :imports, :file
-  end
-
-  def self.down
-    change_table :imports do |t|
-      t.has_attached_file :file
-    end
+  def change
   end
 end
