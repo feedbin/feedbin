ae8e6ced099189c923155ef5dd86b70fc8027eb1
Igor Zubkov <igor.zubkov@gmail.com>
2018-06-08 20:49:27 +0300

    Fix migrations

--- db/migrate/20121019150248_remove_entry_id_from_entries.rb
+++ db/migrate/20121019150248_remove_entry_id_from_entries.rb
@@ -1,5 +1,6 @@
 class RemoveEntryIdFromEntries < ActiveRecord::Migration[4.2]
   def up
+    remove_index :entries, name: :index_entries_on_feed_id_and_entry_id
     remove_index :entries, :entry_id
     remove_column :entries, :entry_id
   end
@@ -7,7 +8,6 @@ class RemoveEntryIdFromEntries < ActiveRecord::Migration[4.2]
   def down
     add_column :entries, :entry_id, :string
     add_index :entries, :entry_id
+    add_index :entries, [:feed_id, :entry_id], unique: true
   end
-
-
 end
--- db/migrate/20121115044716_remove_feed_id_and_entry_id_index_from_entries.rb
+++ db/migrate/20121115044716_remove_feed_id_and_entry_id_index_from_entries.rb
@@ -1,10 +1,4 @@
 class RemoveFeedIdAndEntryIdIndexFromEntries < ActiveRecord::Migration[4.2]
-  def up
-    if index_name_exists?(:entries, :index_entries_on_feed_id_and_entry_id)
-      remove_index :entries, name: :index_entries_on_feed_id_and_entry_id
-    end
-  end
-
-  def down
+  def change
   end
 end
