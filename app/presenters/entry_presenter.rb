53420e58140c32e9d8ecd64634b3a2cc93c1f3b0
Ben Ubois <ben@benubois.com>
2018-08-12 04:15:41 -0700

    Removed unused behavior. CSS refactor.

--- app/presenters/entry_presenter.rb
+++ app/presenters/entry_presenter.rb
@@ -581,7 +581,7 @@ class EntryPresenter < BasePresenter
     url = url.expanded_url.to_s
     if INSTAGRAM_URLS.find { |format| url =~ format } && $1
       instagram_id = $1
-      @template.link_to url, class: "content-styles entry-type-default", target: "_blank", data: {behavior: "format_images"} do
+      @template.link_to url, class: "content-styles entry-type-default", target: "_blank" do
         @template.image_tag("https://instagram.com/p/#{instagram_id}/media/?size=l")
       end
     else
