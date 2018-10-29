dc13d5d4b8b17ba4f90b1e68cb825b0b6f5ffbb7
Ben Ubois <ben@benubois.com>
2018-10-29 13:12:51 -0700

    Fix for increment.

--- app/models/throttle.rb
+++ app/models/throttle.rb
@@ -1,5 +1,6 @@
 class Throttle
   def self.throttle!(key, limit, period, &block)
+    Rails.cache.fetch(key, raw: true) { 1 }
     count = Rails.cache.increment(key, 1, expires_in: period.to_i, initial: 1)
     if count <= limit
       yield
