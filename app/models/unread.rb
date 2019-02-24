3ad169afa0e50f71d9bab11805ddaf7f31dd3e8b
Ben Ubois <ben@benubois.com>
2019-02-24 15:12:59 -0800

    Update table names.

--- app/models/unread.rb
+++ app/models/unread.rb
@@ -3,6 +3,8 @@ class Unread < ApplicationRecord
   belongs_to :feed
   belongs_to :entry
 
+  self.table_name = "unread_entries"
+
   def self.new_from_owners(user, entry)
     new(user_id: user.id, feed_id: entry.feed_id, entry_id: entry.id, published: entry.published, entry_created_at: entry.created_at)
   end
