d0b579e0d1ac5e1d844ccf453df2545e47f870d6
Ben Ubois <ben@benubois.com>
2019-03-14 13:44:08 -0700

    Only count active subs.

--- app/jobs/cache_extracted_content.rb
+++ app/jobs/cache_extracted_content.rb
@@ -2,7 +2,7 @@ class CacheExtractedContent
   include Sidekiq::Worker
 
   def perform(entry_id, feed_id)
-    sticky_on = Subscription.where(feed_id: feed_id, view_inline: true).exists?
+    sticky_on = Subscription.where(feed_id: feed_id, view_inline: true, active: true).exists?
     if sticky_on
       Librato.increment "readability.cache_extract"
     end
