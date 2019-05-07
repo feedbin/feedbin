bed45bf94f295c5473c334a23f3e2629e90cae24
Ben Ubois <ben@benubois.com>
2019-05-07 21:56:59 +0200

    Misc updates.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
       end
     else
       flash.now.alert = "Invalid email or password"
-      render "new"
+      render "new", status: :unauthorized
     end
   end
 
