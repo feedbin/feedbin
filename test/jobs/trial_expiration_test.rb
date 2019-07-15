2837aac973770db9b1cb4d0b5b6fcfab6f316743
Ben Ubois <ben@benubois.com>
2019-07-15 16:41:41 +0200

    Skip unused tests.

--- test/jobs/trial_expiration_test.rb
+++ test/jobs/trial_expiration_test.rb
@@ -14,32 +14,32 @@ class TrialExpirationTest < ActiveSupport::TestCase
     StripeMock.stop
   end
 
-  test "should deactivate timed users" do
-    StripeMock.start
-    create_stripe_plan(plans(:timed))
-    user = users(:timed)
-    user.expires_at = Time.now
-    user.save(validate: false)
-
-    assert_not user.suspended
-    assert_difference -> { Sidekiq::Extensions::DelayedMailer.jobs.size }, +1 do
-      TrialExpiration.new.perform
-    end
-    assert user.reload.suspended
-
-    StripeMock.stop
-  end
-
-  test "should not deactivate timed users with time remaining" do
-    StripeMock.start
-    create_stripe_plan(plans(:timed))
-    user = users(:timed)
-    user.expires_at = Time.now + 5.seconds
-    user.save(validate: false)
-
-    assert_not user.suspended
-    TrialExpiration.new.perform
-    assert_not user.reload.suspended
-    StripeMock.stop
-  end
+  # test "should deactivate timed users" do
+  #   StripeMock.start
+  #   create_stripe_plan(plans(:timed))
+  #   user = users(:timed)
+  #   user.expires_at = Time.now
+  #   user.save(validate: false)
+  #
+  #   assert_not user.suspended
+  #   assert_difference -> { Sidekiq::Extensions::DelayedMailer.jobs.size }, +1 do
+  #     TrialExpiration.new.perform
+  #   end
+  #   assert user.reload.suspended
+  #
+  #   StripeMock.stop
+  # end
+  #
+  # test "should not deactivate timed users with time remaining" do
+  #   StripeMock.start
+  #   create_stripe_plan(plans(:timed))
+  #   user = users(:timed)
+  #   user.expires_at = Time.now + 5.seconds
+  #   user.save(validate: false)
+  #
+  #   assert_not user.suspended
+  #   TrialExpiration.new.perform
+  #   assert_not user.reload.suspended
+  #   StripeMock.stop
+  # end
 end
