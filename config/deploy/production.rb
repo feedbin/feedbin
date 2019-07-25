e3fb51552acbb697480121d0209989373960d0f3
Ben Ubois <ben@benubois.com>
2019-07-25 16:49:24 +0200

    Misc updates.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server "app1.feedbin.com", user: "app", roles: %w[app db]
-server "app2.feedbin.com", user: "app", roles: %w[app]
+server "app1.feedbin.cloud", user: "app", roles: %w[app db]
+server "app2.feedbin.cloud", user: "app", roles: %w[app]
