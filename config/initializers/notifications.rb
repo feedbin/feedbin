05a9bd648cb2132d3447ac68b24ff2ae9d2096d7
Ben Ubois <ben@benubois.com>
2019-11-09 16:50:24 +0100

    Stop recording 99th percentile.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -4,18 +4,18 @@ ActiveSupport::Notifications.subscribe("process_action.action_controller") do |n
     action = payload[:params]["action"]
     if finish && start
       time = (finish - start) * 1000
-      Librato.timing "controller.#{controller}.#{action}.time", time, percentile: [95, 99]
+      Librato.timing "controller.#{controller}.#{action}.time", time, percentile: [95]
       if payload[:params].present? && payload[:params]["subdomain"].present? && payload[:params]["subdomain"] == "api"
-        Librato.timing "response_time.api", time, source: Socket.gethostname, percentile: [95, 99]
+        Librato.timing "response_time.api", time, source: Socket.gethostname, percentile: [95]
       else
-        Librato.timing "response_time.web", time, source: Socket.gethostname, percentile: [95, 99]
+        Librato.timing "response_time.web", time, source: Socket.gethostname, percentile: [95]
       end
     end
     if payload[:db_runtime]
-      Librato.timing "controller.#{controller}.#{action}.time.db", payload[:db_runtime], percentile: [95, 99]
+      Librato.timing "controller.#{controller}.#{action}.time.db", payload[:db_runtime], percentile: [95]
     end
     if payload[:view_runtime]
-      Librato.timing "controller.#{controller}.#{action}.time.view", payload[:view_runtime], percentile: [95, 99]
+      Librato.timing "controller.#{controller}.#{action}.time.view", payload[:view_runtime], percentile: [95]
     end
   end
 end
