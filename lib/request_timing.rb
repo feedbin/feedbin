4ae6c99526d9aca38abf8c74dfdff84d83912cba
Ben Ubois <ben@benubois.com>
2019-12-16 14:53:52 +0100

    Higher resolution timestamp.

--- lib/request_timing.rb
+++ lib/request_timing.rb
@@ -4,8 +4,8 @@ class RequestTiming
   end
 
   def call(env)
-    start = (env["HTTP_X_REQUEST_START"] && env["HTTP_X_REQUEST_START"].respond_to?(:to_i)) ? env["HTTP_X_REQUEST_START"].to_i : Time.now.to_i
-    received = Time.now.to_i
+    received = Time.now.to_f
+    start = (env["HTTP_X_REQUEST_START"] && env["HTTP_X_REQUEST_START"].respond_to?(:to_f)) ? env["HTTP_X_REQUEST_START"].to_f : received
     @app.call(env.merge!({
       "request_timing.start" => start,
       "request_timing.received" => received,
