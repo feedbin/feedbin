b4bb9a4f68dbd367008e66c0dadfd4d3f36a1188
Ben Ubois <ben@benubois.com>
2019-12-23 18:04:38 +0100

    Asset update.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
