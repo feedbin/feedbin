7c67a251f90e391816f41655de991746fb743a60
Ben Ubois <ben@benubois.com>
2019-12-23 20:45:50 +0100

    Bump assets.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :log_level, :warn
 
 # Rails
 set :assets_roles, [:app]
-set :keep_assets, 2
+set :keep_assets, 10
 set :conditionally_migrate, true
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
