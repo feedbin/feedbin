c05a11421bd98565e5def4a2110ab96efb1de3e2
angristan <angristan@pm.me>
2020-02-06 12:45:33 +0100

    S3: configurable region from ENV

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -4,6 +4,7 @@ if ENV["AWS_ACCESS_KEY_ID"]
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_S3_REGION"] || "us-east-1",
       persistent: true,
     )
   }
