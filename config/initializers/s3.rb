b8bea9c55b6de6fcbdd21d4e210349250c6211ef
angristan <angristan@pm.me>
2020-02-06 12:55:59 +0100

    S3: configurable endpoint from ENV

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -5,6 +5,7 @@ if ENV["AWS_ACCESS_KEY_ID"]
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: ENV["AWS_S3_REGION"] || "us-east-1",
+      host: ENV["AWS_HOST"] || "s3.amazonaws.com",
       persistent: true,
     )
   }
