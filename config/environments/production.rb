7a4997e96992bd3048673053be02224acdea91dc
angristan <angristan@pm.me>
2020-02-06 12:24:36 +0100

    Force HTTPS and HSTS from ENV
    Fix #215

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = (ENV["FORCE_SSL"] == "false") ? false : true
   config.ssl_options = {hsts: {subdomains: false}}
 
   # Less verbose logs
