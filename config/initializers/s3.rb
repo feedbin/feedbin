1c53614bfcfae5be529c7e4e9209c4292cae5289
angristan <angristan@pm.me>
2020-02-09 00:24:24 +0100

    Rename AWS_HOST to AWS_S3_HOST for consistency
    Was already AWS_S3_HOST in .env.example

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -5,7 +5,7 @@ if ENV["AWS_ACCESS_KEY_ID"]
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: ENV["AWS_S3_REGION"] || "us-east-1",
-      host: ENV["AWS_HOST"] || "s3.amazonaws.com",
+      host: ENV["AWS_S3_HOST"] || "s3.amazonaws.com",
       endpoint: ENV["AWS_S3_ENDPOINT"] || nil,
       path_style: ENV["AWS_S3_PATH_STYLE"] || false,
       persistent: true,
