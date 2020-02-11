408dd6000326ce99f07fc384625cc4592547e1d7
Ben Ubois <ben@benubois.com>
2020-02-11 21:56:26 +0100

    Fix S3 defaults.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 if ENV["AWS_ACCESS_KEY_ID"] && ENV["AWS_SECRET_ACCESS_KEY"]
   CarrierWave.configure do |config|
-    config.fog_credentials = {
+    options = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_S3_REGION"] || "us-east-1",
-      host: ENV["AWS_S3_HOST"] || "s3.amazonaws.com",
-      endpoint: ENV["AWS_S3_ENDPOINT"] || nil,
-      path_style: ENV["AWS_S3_PATH_STYLE"] || false,
     }
+    options[:region] = ENV["AWS_S3_REGION"] if ENV["AWS_S3_REGION"]
+    options[:host] = ENV["AWS_S3_HOST"] if ENV["AWS_S3_HOST"]
+    options[:endpoint] = ENV["AWS_S3_ENDPOINT"] if ENV["AWS_S3_ENDPOINT"]
+    options[:path_style] = ENV["AWS_S3_PATH_STYLE"] if ENV["AWS_S3_PATH_STYLE"]
+    config.fog_credentials = options
     config.fog_directory = ENV["AWS_S3_BUCKET"]
     config.max_file_size = 5.megabytes
   end
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,14 +1,15 @@
 if ENV["AWS_ACCESS_KEY_ID"]
   S3_POOL = ConnectionPool.new(size: 10, timeout: 5) {
-    Fog::Storage.new(
+    options = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_S3_REGION"] || "us-east-1",
-      host: ENV["AWS_S3_HOST"] || "s3.amazonaws.com",
-      endpoint: ENV["AWS_S3_ENDPOINT"] || nil,
-      path_style: ENV["AWS_S3_PATH_STYLE"] || false,
       persistent: true,
-    )
+    }
+    options[:region] = ENV["AWS_S3_REGION"] if ENV["AWS_S3_REGION"]
+    options[:host] = ENV["AWS_S3_HOST"] if ENV["AWS_S3_HOST"]
+    options[:endpoint] = ENV["AWS_S3_ENDPOINT"] if ENV["AWS_S3_ENDPOINT"]
+    options[:path_style] = ENV["AWS_S3_PATH_STYLE"] if ENV["AWS_S3_PATH_STYLE"]
+    Fog::Storage.new(options)
   }
 end
