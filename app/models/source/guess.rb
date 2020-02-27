d51effaf66ec79ea66cafb3bd63182a46236f886
Ben Ubois <ben@benubois.com>
2020-02-27 17:36:42 +0100

    Always guess a feed address.

--- app/models/source/guess.rb
+++ app/models/source/guess.rb
@@ -2,13 +2,13 @@ class Source::Guess < Source
   def call
     if @config[:request].format == :html
       host = URI.parse(@config[:request].last_effective_url).host
-      if /tumblr\.com/i.match?(@config[:request].body)
-        url = URI::HTTP.build(host: host, path: "/rss").to_s
-        @feed_options.push(FeedOption.new(url, url, url, "guess"))
-      elsif /wordpress/i.match?(@config[:request].body)
-        url = URI::HTTP.build(host: host, path: "/feed").to_s
-        @feed_options.push(FeedOption.new(url, url, url, "guess"))
-      end
+
+      rss = URI::HTTP.build(host: host, path: "/rss").to_s
+      @feed_options.push(FeedOption.new(rss, rss, rss, "guess"))
+
+      feed = URI::HTTP.build(host: host, path: "/feed").to_s
+      @feed_options.push(FeedOption.new(feed, feed, feed, "guess"))
+
       create_feeds!
     end
   end
