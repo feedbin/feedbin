f0f4ed828b2b23eea6f0389c257eadb49d5ace77
Ben Ubois <ben@benubois.com>
2020-05-18 18:59:33 +0200

    Remove title from allowed Evernote tags.

--- app/models/content_formatter.rb
+++ app/models/content_formatter.rb
@@ -103,9 +103,9 @@ class ContentFormatter
     elements: %w[
       a abbr acronym address area b bdo big blockquote br caption center cite code col colgroup dd
       del dfn div dl dt em font h1 h2 h3 h4 h5 h6 hr i img ins kbd li map ol p pre q s samp small
-      span strike strong sub sup table tbody td tfoot th thead title tr tt u ul var xmp
+      strike strong sub sup table tbody td tfoot th thead tr tt u ul var xmp
     ],
-    remove_contents: ["script", "style", "iframe", "object", "embed"],
+    remove_contents: ["script", "style", "iframe", "object", "embed", "title"],
     attributes: {
       "a" => ["href"],
       "img" => ["src"],
