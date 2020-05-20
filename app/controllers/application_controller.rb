7921a84c3760f072f99643800fe5c94430d1e2b9
Ben Ubois <ben@benubois.com>
2020-05-20 21:11:44 +0200

    Share 404 code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::Base
   def render_404
     respond_to do |format|
       format.any do
-        render "errors/not_found.html.erb", layout: nil, status: :not_found
+        render "errors/not_found.html.erb", layout: nil, status: :not_found, content_type: "text/html"
       end
     end
   end
--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,10 +2,6 @@ class ErrorsController < ApplicationController
   skip_before_action :authorize
 
   def not_found
-    respond_to do |format|
-      format.any do
-        render "errors/not_found.html.erb", layout: nil, status: :not_found
-      end
-    end
+    render_404
   end
 end
