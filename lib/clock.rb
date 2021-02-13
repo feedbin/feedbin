dcd4873b768ffb28958eb515d6ab895bbcfbd16b
Ben Ubois <ben@benubois.com>
2021-02-12 23:18:00 -0800

    Run job more frequently

--- lib/clock.rb
+++ lib/clock.rb
@@ -9,6 +9,10 @@ every(10.seconds, "clockwork.very_frequent") do
   if RedisLock.acquire("clockwork:send_stats:v3", 8)
     SendStats.perform_async
   end
+
+  if RedisLock.acquire("clockwork:warm_cache")
+    WarmCache.perform_async(nil, true)
+  end
 end
 
 every(1.minutes, "clockwork.frequent") do
@@ -18,7 +22,6 @@ every(1.minutes, "clockwork.frequent") do
 
   if RedisLock.acquire("clockwork:harvest:embed:data")
     HarvestEmbedData.perform_async
-    WarmCache.perform_async(nil, true)
   end
 end
 
