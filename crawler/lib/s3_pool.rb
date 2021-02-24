08e87ec33e6d54b4f261d1b44935fe7c034fde0c
Karl Prieb <karl@prieb.me>
2021-02-23 21:15:41 -0300

    S3: Add support for region, storage class and non AWS providers Initial commit made by @angristan https://github.com/angristan

--- crawler/lib/s3_pool.rb
+++ crawler/lib/s3_pool.rb
@@ -1,8 +1,14 @@
 S3_POOL = ConnectionPool.new(size: 10, timeout: 5) do
-  Fog::Storage.new(
+  options = {
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     persistent: true
-  )
+  }
+  options[:region] = ENV["AWS_S3_REGION"] if ENV["AWS_S3_REGION"]
+  options[:host] = ENV["AWS_S3_HOST"] if ENV["AWS_S3_HOST"]
+  options[:endpoint] = ENV["AWS_S3_ENDPOINT"] if ENV["AWS_S3_ENDPOINT"]
+  options[:path_style] = ENV["AWS_S3_PATH_STYLE"] if ENV["AWS_S3_PATH_STYLE"]
+  Fog::Storage.new(options)
 end
+
