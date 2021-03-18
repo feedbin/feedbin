a7d6a0d3a91236360f53c657a1ec9b470bec8898
Ben Ubois <ben@benubois.com>
2021-03-18 13:31:48 -0700

    Stat updates.

--- crawler/lib/worker_stat.rb
+++ crawler/lib/worker_stat.rb
@@ -1,8 +1,8 @@
 class WorkerStat
   def call(worker, item, queue)
     title = "worker.#{worker.class}"
-    Librato.increment "#{title}.count", source: Socket.gethostname
-    Librato.timing title, source: Socket.gethostname do
+    Librato.increment "#{title}.count"
+    Librato.timing title, percentile: [95] do
       yield
     end
   end
