f6d8064cfad41777c96de091e32831c9ca615bd7
Ben Ubois <ben@benubois.com>
2021-03-18 11:55:55 -0700

    Timeout adjustments.

--- crawler/lib/redis.rb
+++
@@ -1,7 +0,0 @@
-$redis = begin
-  if ENV["REDIS_ID_URL"]
-    Redis.new(url: ENV["REDIS_ID_URL"])
-  else
-    Redis.new
-  end
-end
---
+++ crawler/lib/timer.rb
@@ -0,0 +1,22 @@
+class Timer
+  def initialize(timeout = 0)
+    start
+    @deadline = now + timeout.to_f
+  end
+
+  def expired?
+     now > @deadline
+  end
+
+  def now
+    ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+  end
+
+  def elapsed
+    (now - start).ceil(2)
+  end
+
+  def start
+    @start ||= now
+  end
+end
