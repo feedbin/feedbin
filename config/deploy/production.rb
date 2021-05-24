edf2305dbee8698cbd70c78b6f5010b0084717d0
Ben Ubois <ben@benubois.com>
2021-05-23 18:49:41 -0700

    Removed unused files.

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-server "app1.feedbin", user: "app", roles: %w[app db]
-server "app2.feedbin", user: "app", roles: %w[app]
-server "app4.feedbin", user: "app", roles: %w[app]
-server "app5.feedbin", user: "app", roles: %w[app]
