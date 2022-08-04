2ee0a5637baf9a8d17fd9fd563ffd986485c3bc8
Ben Ubois <ben@benubois.com>
2022-08-04 03:28:50 +0200

    Disable compression.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
+  config.assets.js_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
