e30ea6e2357ed2ffa5d5aff61153f361531d9f3a
Ben Ubois <ben@benubois.com>
2022-09-16 22:50:54 +0200

    Renamed files.

--- app/jobs/feed_crawler/schedule_all.rb
+++ app/jobs/feed_crawler/schedule_all.rb
@@ -1,57 +1,58 @@
 require "sidekiq/api"
+module FeedCrawler
+  class ScheduleAll
+    include Sidekiq::Worker
+    include SidekiqHelper
+    sidekiq_options queue: :worker_slow_critical
 
-class FeedRefresherScheduler
-  include Sidekiq::Worker
-  include SidekiqHelper
-  sidekiq_options queue: :worker_slow_critical
+    COUNT_KEY = "feed_refresher_scheduler:count".freeze
 
-  COUNT_KEY = "feed_refresher_scheduler:count".freeze
-
-  def perform
-    if queue_empty?("feed_refresher_receiver") && queue_empty?("feed_downloader")
-      refresh_feeds
-      TwitterFeedRefresher.perform_async if queue_empty?("twitter_refresher")
+    def perform
+      if queue_empty?("feed_refresher_receiver") && queue_empty?("feed_downloader")
+        refresh_feeds
+        TwitterFeedRefresher.perform_async if queue_empty?("twitter_refresher")
+      end
     end
-  end
 
-  def refresh_feeds
-    feed = Feed.last
-    if feed
-      jobs = job_args(feed.id, 1, priority?)
-      Sidekiq::Client.push_bulk(
-        "args" => jobs,
-        "class" => "FeedRefresher",
-        "queue" => "worker_slow_critical"
-      )
-      increment
-      report
+    def refresh_feeds
+      feed = Feed.last
+      if feed
+        jobs = job_args(feed.id, 1, priority?)
+        Sidekiq::Client.push_bulk(
+          "args" => jobs,
+          "class" => "FeedRefresher",
+          "queue" => "worker_slow_critical"
+        )
+        increment
+        report
+      end
     end
-  end
 
-  def priority?
-    @priority ||= count % 2 == 0
-  end
-
-  def increment
-    Librato.increment "refresh_feeds"
-    Sidekiq.redis { |client| client.incr(COUNT_KEY) }
-  end
-
-  def report
-    if ENV["FEED_REFRESHER_REPORT_URL"]
-      HTTP.get(ENV["FEED_REFRESHER_REPORT_URL"])
+    def priority?
+      @priority ||= count % 2 == 0
     end
-  end
 
-  def count
-    @count ||= begin
-      result = Sidekiq.redis { |client| client.get(COUNT_KEY) } || 0
-      result.to_i
+    def increment
+      Librato.increment "refresh_feeds"
+      Sidekiq.redis { |client| client.incr(COUNT_KEY) }
     end
-  end
 
-  def queue_empty?(queue)
-    @queues ||= Sidekiq::Stats.new.queues
-    @queues[queue].blank? || @queues[queue] == 0
+    def report
+      if ENV["FEED_REFRESHER_REPORT_URL"]
+        HTTP.get(ENV["FEED_REFRESHER_REPORT_URL"])
+      end
+    end
+
+    def count
+      @count ||= begin
+        result = Sidekiq.redis { |client| client.get(COUNT_KEY) } || 0
+        result.to_i
+      end
+    end
+
+    def queue_empty?(queue)
+      @queues ||= Sidekiq::Stats.new.queues
+      @queues[queue].blank? || @queues[queue] == 0
+    end
   end
 end
