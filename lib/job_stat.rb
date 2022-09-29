703fe44c6a50b385316223b7a77388cad1131e17
Ben Ubois <ben@benubois.com>
2022-09-29 05:48:56 +0200

    worker -> job. Remove source.

--- lib/job_stat.rb
+++ lib/job_stat.rb
@@ -1,9 +1,9 @@
 class JobStat
-  def call(worker, item, queue)
-    title = "job.#{worker.class.to_s.underscore.parameterize}"
-    Librato.increment "#{title}.count", source: Socket.gethostname
-    Librato.increment "worker_perform", source: Socket.gethostname
-    Librato.timing title, source: Socket.gethostname do
+  def call(job, item, queue)
+    title = "job.#{job.class.to_s.underscore.parameterize}"
+    Librato.increment "#{title}.count"
+    Librato.increment "job.count"
+    Librato.timing title
       yield
     end
   end
