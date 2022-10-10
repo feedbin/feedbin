15d205f6e1e82bf322f5cacbf4b63e7f318ad63a
Ben Ubois <ben@benubois.com>
2022-10-10 19:28:06 +0200

    Rename.

--- app/jobs/favicon/favicon_finder.rb
+++ app/jobs/favicon/favicon_finder.rb
@@ -1,120 +1,120 @@
-require "rmagick"
+module Favicon
+  class Finder
+    include Sidekiq::Worker
+    sidekiq_options retry: false
 
-class FaviconFetcher
-  include Sidekiq::Worker
-  sidekiq_options retry: false
-
-  def perform(host, force = false)
-    @favicon = Favicon.unscoped.where(host: host).first_or_initialize
-    @force = force
-    update if should_update?
-  rescue
-    Librato.increment("favicon.failed")
-  end
-
-  def update
-    favicon_found = false
-    response = nil
-
-    favicon_url = find_favicon_link
-    if favicon_url
-      response = download_favicon(favicon_url)
-      favicon_found = true unless response.to_s.empty?
+    def perform(host, force = false)
+      @favicon = Favicon.unscoped.where(host: host).first_or_initialize
+      @force = force
+      update if should_update?
+    rescue
+      Librato.increment("favicon.failed")
     end
 
-    unless favicon_found
-      favicon_url = default_favicon_location
-      response = download_favicon(favicon_url)
-    end
+    def update
+      favicon_found = false
+      response = nil
 
-    if response
-      processor = FaviconProcessor.new(response.to_s, @favicon.host)
-      if processor.valid? && @favicon.data["favicon_hash"] != processor.favicon_hash
-        @favicon.favicon = processor.encoded_favicon if processor.encoded_favicon
-        @favicon.url = processor.favicon_url if processor.favicon_url
-        @favicon.data = get_data(response, processor.favicon_hash)
-        Librato.increment("favicon.updated")
+      favicon_url = find_favicon_link
+      if favicon_url
+        response = download_favicon(favicon_url)
+        favicon_found = true unless response.to_s.empty?
       end
-      Librato.increment("favicon.status", source: response.code)
+
+      unless favicon_found
+        favicon_url = default_favicon_location
+        response = download_favicon(favicon_url)
+      end
+
+      if response
+        processor = FaviconProcessor.new(response.to_s, @favicon.host)
+        if processor.valid? && @favicon.data["favicon_hash"] != processor.favicon_hash
+          @favicon.favicon = processor.encoded_favicon if processor.encoded_favicon
+          @favicon.url = processor.favicon_url if processor.favicon_url
+          @favicon.data = get_data(response, processor.favicon_hash)
+          Librato.increment("favicon.updated")
+        end
+        Librato.increment("favicon.status", source: response.code)
+      end
+
+      @favicon.save
     end
 
-    @favicon.save
-  end
-
-  def get_data(response, favicon_hash)
-    data = {favicon_hash: favicon_hash}
-    if response
-      data = data.merge!(response.headers.to_h.extract!("Last-Modified", "Etag"))
+    def get_data(response, favicon_hash)
+      data = {favicon_hash: favicon_hash}
+      if response
+        data = data.merge!(response.headers.to_h.extract!("Last-Modified", "Etag"))
+      end
+      data
     end
-    data
-  end
 
-  def find_favicon_link
-    favicon_url = nil
-    url = URI::HTTP.build(host: @favicon.host)
-    response = HTTP
-      .timeout(write: 5, connect: 5, read: 5)
-      .follow
-      .get(url)
-      .to_s
-    html = Nokogiri::HTML(response)
-    favicon_links = html.search(xpath)
-    if favicon_links.present?
-      favicon_url = favicon_links.first.to_s
-      favicon_url = URI.parse(favicon_url)
-      favicon_url.scheme = "http"
-      unless favicon_url.host
-        favicon_url = URI::HTTP.build(scheme: "http", host: @favicon.host)
-        favicon_url = favicon_url.merge(favicon_links.last.to_s)
+    def find_favicon_link
+      favicon_url = nil
+      url = URI::HTTP.build(host: @favicon.host)
+      response = HTTP
+        .timeout(write: 5, connect: 5, read: 5)
+        .follow
+        .get(url)
+        .to_s
+      html = Nokogiri::HTML(response)
+      favicon_links = html.search(xpath)
+      if favicon_links.present?
+        favicon_url = favicon_links.first.to_s
+        favicon_url = URI.parse(favicon_url)
+        favicon_url.scheme = "http"
+        unless favicon_url.host
+          favicon_url = URI::HTTP.build(scheme: "http", host: @favicon.host)
+          favicon_url = favicon_url.merge(favicon_links.last.to_s)
+        end
+      end
+      favicon_url
+    rescue
+      nil
+    end
+
+    def default_favicon_location
+      URI::HTTP.build(host: @favicon.host, path: "/favicon.ico")
+    end
+
+    def download_favicon(url)
+      response = HTTP
+        .timeout(write: 5, connect: 5, read: 5)
+        .follow
+        .headers(request_headers)
+        .get(url)
+    end
+
+    def request_headers
+      headers = {user_agent: "Mozilla/5.0"}
+      unless @force
+        conditional_headers = ConditionalHttp.new(@favicon.data["Etag"], @favicon.data["Last-Modified"])
+        headers = headers.merge(conditional_headers.to_h)
+      end
+      headers
+    end
+
+    def should_update?
+      if @force
+        true
+      else
+        !updated_recently?
       end
     end
-    favicon_url
-  rescue
-    nil
-  end
 
-  def default_favicon_location
-    URI::HTTP.build(host: @favicon.host, path: "/favicon.ico")
-  end
-
-  def download_favicon(url)
-    response = HTTP
-      .timeout(write: 5, connect: 5, read: 5)
-      .follow
-      .headers(request_headers)
-      .get(url)
-  end
-
-  def request_headers
-    headers = {user_agent: "Mozilla/5.0"}
-    unless @force
-      conditional_headers = ConditionalHttp.new(@favicon.data["Etag"], @favicon.data["Last-Modified"])
-      headers = headers.merge(conditional_headers.to_h)
+    def updated_recently?
+      if @favicon.updated_at
+        @favicon.updated_at > 1.hour.ago
+      else
+        false
+      end
     end
-    headers
-  end
 
-  def should_update?
-    if @force
-      true
-    else
-      !updated_recently?
+    def xpath
+      icon_names = ["shortcut icon", "icon", "apple-touch-icon", "apple-touch-icon-precomposed"]
+      icon_names = icon_names.map { |icon_name|
+        "//link[not(@mask) and translate(@rel, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = '#{icon_name}']/@href"
+      }
+      icon_names.join(" | ")
     end
-  end
-
-  def updated_recently?
-    if @favicon.updated_at
-      @favicon.updated_at > 1.hour.ago
-    else
-      false
-    end
-  end
-
-  def xpath
-    icon_names = ["shortcut icon", "icon", "apple-touch-icon", "apple-touch-icon-precomposed"]
-    icon_names = icon_names.map { |icon_name|
-      "//link[not(@mask) and translate(@rel, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = '#{icon_name}']/@href"
-    }
-    icon_names.join(" | ")
   end
 end
--- app/jobs/favicon/favicon_processor.rb
+++ app/jobs/favicon/favicon_processor.rb
@@ -1,95 +1,97 @@
-class FaviconProcessor
-  attr_reader :data, :host
+module Favicon
+  class Processor
+    attr_reader :data, :host
 
-  AWS_S3_BUCKET_FAVICONS = ENV["AWS_S3_BUCKET_FAVICONS"] || ENV["AWS_S3_BUCKET"]
+    AWS_S3_BUCKET_FAVICONS = ENV["AWS_S3_BUCKET_FAVICONS"] || ENV["AWS_S3_BUCKET"]
 
-  def initialize(data, host)
-    @data = data
-    @host = host
-  end
-
-  def favicon_url
-    @favicon_url ||= upload(image_data)
-  end
-
-  def encoded_favicon
-    @encoded_favicon ||= encoded(image_data)
-  end
-
-  def valid?
-    data.present?
-  end
-
-  def favicon_hash
-    @favicon_hash ||= Digest::SHA1.hexdigest(data)
-  end
-
-  def upload_existing
-    upload(data)
-  end
-
-  private
-
-  def upload(data)
-    upload_url = nil
-    S3_POOL.with do |connection|
-      response = connection.put_object(AWS_S3_BUCKET_FAVICONS, File.join(favicon_hash[0..2], "#{favicon_hash}.png"), data, s3_options)
-      upload_url = URI::HTTP.build(
-        scheme: "https",
-        host: response.data[:host],
-        path: response.data[:path]
-      ).to_s
+    def initialize(data, host)
+      @data = data
+      @host = host
     end
-    upload_url
-  end
 
-  def encoded(blob)
-    Base64.encode64(blob).delete("\n")
-  end
-
-  def image_data
-    @image_data ||= get_image_data
-  end
-
-  def get_image_data
-    layers = load_layers
-    favicon = remove_blank_images(layers).last
-    if favicon.columns > 32
-      favicon = favicon.resize_to_fit(32, 32)
+    def favicon_url
+      @favicon_url ||= upload(image_data)
     end
-    favicon.to_blob { |image| image.format = "png" }
-  ensure
-    favicon&.destroy!
-    layers&.map(&:destroy!)
-  end
 
-  def load_layers
-    Magick::Image.from_blob(data)
-  rescue Magick::ImageMagickError
-    Magick::Image.from_blob(data) { |image| image.format = "ico" }
-  end
-
-  def remove_blank_images(layers)
-    layers.reject do |layer|
-      layer = layer.scale(1, 1)
-      pixel = layer.pixel_color(0, 0)
-      color = layer.to_color(pixel)
-      %w[none white].include?(color) || color.include?("#FFFFFF")
+    def encoded_favicon
+      @encoded_favicon ||= encoded(image_data)
     end
-  end
 
-  def s3_options
-    {
-      "Content-Type" => "image/png",
-      "Cache-Control" => "max-age=315360000, public",
-      "Expires" => "Sun, 29 Jun 2036 17:48:34 GMT",
-      "x-amz-acl" => "public-read",
-      "x-amz-storage-class" => ENV["AWS_S3_STORAGE_CLASS"] || "REDUCED_REDUNDANCY",
-      "x-amz-meta-favicon-host" => host
-    }
-  end
+    def valid?
+      data.present?
+    end
 
-  def path
-    @path ||= File.join("public-favicons", favicon_hash[0..3], "#{favicon_hash}.png")
+    def favicon_hash
+      @favicon_hash ||= Digest::SHA1.hexdigest(data)
+    end
+
+    def upload_existing
+      upload(data)
+    end
+
+    private
+
+    def upload(data)
+      upload_url = nil
+      S3_POOL.with do |connection|
+        response = connection.put_object(AWS_S3_BUCKET_FAVICONS, File.join(favicon_hash[0..2], "#{favicon_hash}.png"), data, s3_options)
+        upload_url = URI::HTTP.build(
+          scheme: "https",
+          host: response.data[:host],
+          path: response.data[:path]
+        ).to_s
+      end
+      upload_url
+    end
+
+    def encoded(blob)
+      Base64.encode64(blob).delete("\n")
+    end
+
+    def image_data
+      @image_data ||= get_image_data
+    end
+
+    def get_image_data
+      layers = load_layers
+      favicon = remove_blank_images(layers).last
+      if favicon.columns > 32
+        favicon = favicon.resize_to_fit(32, 32)
+      end
+      favicon.to_blob { |image| image.format = "png" }
+    ensure
+      favicon&.destroy!
+      layers&.map(&:destroy!)
+    end
+
+    def load_layers
+      Magick::Image.from_blob(data)
+    rescue Magick::ImageMagickError
+      Magick::Image.from_blob(data) { |image| image.format = "ico" }
+    end
+
+    def remove_blank_images(layers)
+      layers.reject do |layer|
+        layer = layer.scale(1, 1)
+        pixel = layer.pixel_color(0, 0)
+        color = layer.to_color(pixel)
+        %w[none white].include?(color) || color.include?("#FFFFFF")
+      end
+    end
+
+    def s3_options
+      {
+        "Content-Type" => "image/png",
+        "Cache-Control" => "max-age=315360000, public",
+        "Expires" => "Sun, 29 Jun 2036 17:48:34 GMT",
+        "x-amz-acl" => "public-read",
+        "x-amz-storage-class" => ENV["AWS_S3_STORAGE_CLASS"] || "REDUCED_REDUNDANCY",
+        "x-amz-meta-favicon-host" => host
+      }
+    end
+
+    def path
+      @path ||= File.join("public-favicons", favicon_hash[0..3], "#{favicon_hash}.png")
+    end
   end
 end
