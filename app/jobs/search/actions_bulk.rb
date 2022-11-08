812ff879673099c992e7a07bde0dd8669acf3698
Ben Ubois <ben@benubois.com>
2022-11-08 18:49:31 +0100

    Action updates.

--- app/jobs/search/actions_bulk.rb
+++ app/jobs/search/actions_bulk.rb
@@ -16,21 +16,8 @@ module Search
         end
       end
 
-      action.actions.each do |task|
-        case task
-        when "send_push_notificationx"
-          entry_ids.map do |entry_id|
-            SafariPushNotificationSend.perform_async([user.id], entry_id)
-          end
-        when "star"
-          Entry.where(id: entry_ids).find_in_batches do |entries|
-            entries.each do |entry|
-              StarredEntry.create_from_owners(user, entry, "ActionsExisting: #{action.id}")
-            end
-          end
-        when "mark_read"
-          user.unread_entries.where(entry_id: entry_ids).delete_all
-        end
+      action.actions.include?("mark_read").each do |task|
+        user.unread_entries.where(entry_id: entry_ids).delete_all
       end
     end
   end
