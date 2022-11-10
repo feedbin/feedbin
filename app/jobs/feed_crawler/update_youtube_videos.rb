ad3f780116c2d21e3beeb76d3cd72959a9d7900f
Ben Ubois <ben@benubois.com>
2022-11-10 15:19:14 +0100

    Make sure data is available.

--- app/jobs/feed_crawler/update_youtube_videos.rb
+++ app/jobs/feed_crawler/update_youtube_videos.rb
@@ -6,10 +6,10 @@ module FeedCrawler
     def perform(feed_id)
       feed = Feed.find(feed_id)
       entries = feed.entries
-      ids = entries.map { |entry| entry.data.dig("youtube_video_id") }
+      ids = entries.flat_map { |entry| entry.data&.dig("youtube_video_id") }
       embeds = Embed.youtube_video.where(provider_id: ids).index_by(&:provider_id)
       entries.map do |entry|
-        id = entry.data.dig("youtube_video_id")
+        id = entry.data&.dig("youtube_video_id")
         if embed = embeds[id]
           unless embed.duration_in_seconds == 0
             entry.update(embed_duration: embed.duration_in_seconds)
