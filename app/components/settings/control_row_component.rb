a8b5a1527e97a8829b2d0dbd7b1a25f103a51c03
Ben Ubois <ben@benubois.com>
2022-11-23 14:58:40 +0100

    Sharing services.

--- app/components/settings/control_row_component.rb
+++ app/components/settings/control_row_component.rb
@@ -2,4 +2,5 @@ class Settings::ControlRowComponent < BaseComponent
   renders_one :title
   renders_one :description
   renders_one :control
+  renders_one :icon
 end
--- app/components/settings/expandable_component.rb
+++ app/components/settings/expandable_component.rb
@@ -2,4 +2,8 @@ class Settings::ExpandableComponent < BaseComponent
   renders_one :header, "Settings::SectionHeaderComponent"
   renders_one :description
   renders_many :items
+
+  def initialize(capsule: false)
+    @capsule = capsule
+  end
 end
