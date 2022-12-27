d2680ca9418aa18746dd9421d98399362d372b8a
Ben Ubois <ben@benubois.com>
2022-12-27 18:49:29 +0100

    Add source to job stats.

--- lib/job_stat.rb
+++ lib/job_stat.rb
@@ -3,7 +3,7 @@ class JobStat
     title = "job.#{job.class.to_s.underscore.parameterize}"
     Librato.increment "#{title}.count"
     Librato.increment "job.count"
-    Librato.timing title do
+    Librato.timing title, source: Socket.gethostname do
       yield
     end
   end
