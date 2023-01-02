54eb924c5a6c0d8fc53ed091e2e84ff22c77a294
Ben Ubois <ben@benubois.com>
2023-01-02 14:50:19 +0100

    Provider fixes.

--- app/jobs/backfill_provider_ids.rb
+++ app/jobs/backfill_provider_ids.rb
@@ -5,7 +5,7 @@ class BackfillProviderIds
 
   def perform(batch)
     ids = build_ids(batch)
-    entries = Entry.where(id: ids).select(:id, :data, :main_tweet_id)
+    entries = Entry.where(id: ids).select(:id, :data, :main_tweet_id, :image)
 
     provider_parent_id_data = {}
     provider_id_data = {}
@@ -14,9 +14,10 @@ class BackfillProviderIds
     video_map = {}
 
     entries.each do |entry, hash|
-      if id = entry.main_tweet_id
+      if entry.tweet? && id = entry.tweet.main_tweet.user.screen_name
         provider_id_data[entry.id] = id
         provider_data[entry.id] = 0
+        provider_parent_id_data[entry.id] = id
       elsif id = entry.data.safe_dig("youtube_video_id")
         provider_id_data[entry.id] = id
         provider_data[entry.id] = 1
@@ -37,6 +38,7 @@ class BackfillProviderIds
       Entry.update_multiple(column: :provider_id, data: provider_id_data)
       Entry.update_multiple(column: :provider, data: provider_data)
     end
+
     if provider_parent_id_data.present?
       Entry.update_multiple(column: :provider_parent_id, data: provider_parent_id_data)
     end
