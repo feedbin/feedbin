84c8c97939d9f1ffe2aeeb63af6cae62e55b03bb
Ben Ubois <ben@benubois.com>
2023-01-16 13:49:16 +0100

    Simplify expandable.

--- app/javascript/controllers/expandable_controller.js
+++ app/javascript/controllers/expandable_controller.js
@@ -2,19 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="expandable"
 export default class extends Controller {
-  static targets = ["container", "content"];
   static values = {
     open: Boolean
   }
 
   toggle(event) {
     this.openValue = !this.openValue;
-    const height = this.openValue ? this.contentTarget.getBoundingClientRect().height : 0
-    this.containerTarget.style["max-height"] = `${height}px`
-    const tabTargets = this.contentTarget.querySelectorAll("[tabindex]")
-
-    tabTargets.forEach((element) => {
-      element.tabIndex = this.openValue ? "0" : "-1"
-    })
   }
 }
