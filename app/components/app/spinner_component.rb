5295be5273a626fb3b57356f859200f1eb579de6
Ben Ubois <ben@benubois.com>
2023-02-20 13:30:58 +0100

    Hide spinner fully when not in use.

--- app/components/app/spinner_component.rb
+++ app/components/app/spinner_component.rb
@@ -1,7 +1,7 @@
 class App::SpinnerComponent < BaseComponent
 
   def call
-    content_tag :div, class: "flex flex-center w-full h-full transition opacity-0 group-data-[processing=true]:opacity-100" do
+    content_tag :div, class: "flex-center w-full h-full transition opacity-0 tw-hidden group-data-[processing=true]:opacity-100 group-data-[processing=true]:flex" do
       content_tag :div, "", class: "spinner small"
     end
   end
