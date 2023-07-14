29c09210484f95691429e8e0432c8648f1bad33c
MaciaAtalis <macia@atalisfunding.com>
2023-07-14 12:23:38 +0200

    architecture done The relationships between users, r_users_profiles, profiles, r_profiles_tags, tags are working.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,8 @@
 class Profile < ApplicationRecord
+    
+    has_many :r_users_profiles
+    has_many :users, through: :r_users_profiles
+
+    has_many :r_profiles_tags
+    has_many :tags, through: :r_profiles_tags
 end
