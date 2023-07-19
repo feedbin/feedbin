623809cd23eec083358e8ae5f818eda3d08538a3
MaciaAtalis <macia@atalisfunding.com>
2023-07-19 08:22:59 +0200

    Adding relations with User-Profile-Tags

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,10 @@
 class Profile < ApplicationRecord
+
+    def assing_profile_to_user(user_id, profile_id)
+        RUsersProfile( user_id: user_id, profile_id: profile_id).save
+    end
+
+    def assign_profile_to_tag(user_id, tag_id)
+        RProfilesTag( profile_id: user_id, tag_id: tag_id).save
+    end
 end
