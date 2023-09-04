de3a89f4eb3739d09e6d192b69c98f943af1079c
Ben Ubois <ben@benubois.com>
2023-09-03 21:04:21 -0700

    Turn on server timing.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,4 +131,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.server_timing = true
 end
