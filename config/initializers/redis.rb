4fa256a3dc7ffc407a932bca5891e07d3110fd52
Ben Ubois <ben@benubois.com>
2023-10-11 20:40:39 +0200

    Revert redis initializer change.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,10 @@
 defaults = {connect_timeout: 5, timeout: 5}
 defaults[:url] = ENV["REDIS_URL"] if ENV["REDIS_URL"]
 
-unless Rails.env.test?
-  $redis = {}.tap do |hash|
-    options2 = defaults.dup
-    if ENV["REDIS_URL_PUBLIC_IDS"] || ENV["REDIS_URL_CACHE"]
-      options2[:url] = ENV["REDIS_URL_PUBLIC_IDS"] || ENV["REDIS_URL_CACHE"]
-    end
-    hash[:refresher] = ConnectionPool.new(size: 10) { Redis.new(options2) }
+$redis = {}.tap do |hash|
+  options2 = defaults.dup
+  if ENV["REDIS_URL_PUBLIC_IDS"] || ENV["REDIS_URL_CACHE"]
+    options2[:url] = ENV["REDIS_URL_PUBLIC_IDS"] || ENV["REDIS_URL_CACHE"]
   end
+  hash[:refresher] = ConnectionPool.new(size: 10) { Redis.new(options2) }
 end
