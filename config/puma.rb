dd86aa2fb1e8073c5cab802c058e782a31f055af
Ben Ubois <ben@benubois.com>
2023-10-19 07:35:56 +0200

    Puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,35 +1,30 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+require "etc"
+require "dotenv"
+require "honeybadger"
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+
+workers ENV.fetch("WEB_CONCURRENCY") { "2" }
 threads min_threads_count, max_threads_count
+environment ENV.fetch("RAILS_ENV", "development")
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
+if @options[:environment] == "production"
+  shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
+  shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
+  pidfile File.join(shared_directory, "tmp", "puma.pid")
+  bind    File.join("unix://", shared_directory, "tmp", "puma.sock")
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-
-# Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_booted do
+  ENV.update Dotenv.load
+end
+
+lowlevel_error_handler do |exception|
+  Honeybadger.notify(exception)
+  [500, {}, ["An unknown error has occurred. Please contact #{ENV["FROM_ADDRESS"]} for support."]]
+end
