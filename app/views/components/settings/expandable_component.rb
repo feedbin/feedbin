72daa92851a11dac6f4325230c90e5bdd0825be1
Ben Ubois <ben@benubois.com>
2023-10-24 19:42:20 +0200

    Extract expandable.

--- app/views/components/settings/expandable_component.rb
+++ app/views/components/settings/expandable_component.rb
@@ -9,13 +9,15 @@ module Settings
     end
 
     def template
-      render Settings::ControlGroupComponent.new(**attributes) do |group|
-        group.header(&@header)
-        group.item(&@description)
-        group.item do
-          div class: "grid [grid-template-rows:0fr] group-data-[expandable-open-value=true]:[grid-template-rows:1fr] transition-[grid-template-rows] duration-200 overflow-hidden" do
-            ul class: "min-h-0 border-t transition transition-[visibility,opacity] opacity-100 group-data-[expandable-open-value=false]:opacity-0 group-data-[expandable-open-value=false]:invisible" do
-              @items.each {render _1}
+      render App::ExpandableContainerComponent.new do |expandable|
+        render Settings::ControlGroupComponent.new(**attributes) do |group|
+          group.header(&@header)
+          group.item(&@description)
+          group.item do
+            expandable.content do
+              ul class: "border-t" do
+                @items.each {render _1}
+              end
             end
           end
         end
@@ -29,7 +31,7 @@ module Settings
     private
 
     def attributes
-      mix({class: "group [&_[data-item]]:border-0", data: { controller: "expandable", expandable_open_value: "false" }}, @attributes)
+      mix({class: "group [&_[data-item]]:border-0"}, @attributes)
     end
 
     class ItemComponent < ApplicationComponent
