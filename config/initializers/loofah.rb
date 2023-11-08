2cbbaed23692b66a616d2e38324b6ca6704e20f7
Ben Ubois <ben@benubois.com>
2023-11-08 14:31:59 +0100

    Allow some app protocols.

--- config/initializers/loofah.rb
+++ config/initializers/loofah.rb
@@ -1,3 +1,14 @@
 Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS_WITH_LIBXML2.merge %w[
-  iframe picture source
+  iframe
+  picture
+  source
+]
+Loofah::HTML5::WhiteList::ACCEPTABLE_PROTOCOLS.merge %w[
+  apollo
+  drafts
+  ivory
+  omnifocus
+  pocket
+  things
+  todoist
 ]
