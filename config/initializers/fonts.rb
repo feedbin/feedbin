cb7b14ded0c1475f32c3acbbc93da3ee658318e2
Ben Ubois <ben@benubois.com>
2024-02-21 17:04:04 +0100

    Offer ui-serif font option.

--- config/initializers/fonts.rb
+++ config/initializers/fonts.rb
@@ -2,10 +2,10 @@ Rails.application.reloader.to_prepare do
   Feedbin::Application.config.font_sizes = (1..10).to_a
   Feedbin::Application.config.fonts = [
     Font.new("System", "default"),
-    Font.new("Sentinel", "serif-1"),
+    Font.new("System Serif", "serif-3"),
     Font.new("Whitney", "sans-serif-1"),
-    Font.new("Mercury", "serif-2"),
+    Font.new("Sentinel", "serif-1"),
     Font.new("Ideal Sans", "sans-serif-2"),
-    Font.new("Georgia", "serif-3")
+    Font.new("Mercury", "serif-2"),
   ]
 end
