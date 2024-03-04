6ab7e5751fddf66093c2f3d2211890a55f79177f
Ben Ubois <ben@benubois.com>
2024-03-04 16:37:25 +0100

    New newsletter delivery method.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,15 +1,8 @@
 class NewslettersController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :authorize, only: [:create]
 
   def create
-    newsletter = Newsletter.new(params)
-    user = AuthenticationToken.newsletters.active.where(token: newsletter.token).take&.user
-    if user && newsletter.valid?
-      NewsletterEntry.create(newsletter, user)
-    end
-    active = user ? !user.suspended : false
-    Librato.increment "newsletter.user_active.#{active}"
+    NewsletterProcessor.perform_async(params[:newsletter][:to], params[:newsletter][:url])
     head :ok
   end
 
--- app/jobs/newsletter_saver.rb
+++ app/jobs/newsletter_saver.rb
@@ -9,6 +9,7 @@ class NewsletterSaver
       content = ContentFormatter.text_email(content)
       content = "<h1>#{entry.title}</h1>#{content}"
     end
+    content = ActiveSupport::Gzip.compress(content)
 
     response = Fog::Storage.new(STORAGE).put_object(ENV["AWS_S3_BUCKET_NEWSLETTERS"], File.join(entry.public_id[0..2], "#{entry.public_id}.html"), content, s3_options)
     host = ENV["NEWSLETTER_HOST"] || response.data[:host]
@@ -21,10 +22,11 @@ class NewsletterSaver
 
   def s3_options
     {
-      "Content-Type" => "text/html; charset=utf-8",
-      "Cache-Control" => "max-age=315360000, public",
-      "Expires" => "Sun, 29 Jun 2036 17:48:34 GMT",
-      "x-amz-acl" => "public-read",
+      "Content-Type"        => "text/html; charset=utf-8",
+      "Cache-Control"       => "max-age=315360000, public",
+      "Expires"             => "Sun, 29 Jun 2036 17:48:34 GMT",
+      "Content-Encoding"    => "gzip",
+      "x-amz-acl"           => "public-read",
       "x-amz-storage-class" => ENV["AWS_S3_STORAGE_CLASS"] || "REDUCED_REDUNDANCY"
     }
   end
