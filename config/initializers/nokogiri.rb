a9d273b6097594375d66985f65570341908c2bdf
Ben Ubois <ben@benubois.com>
2024-03-05 14:24:01 +0100

    Increase tree depth.

---
+++ config/initializers/nokogiri.rb
@@ -0,0 +1,4 @@
+# emits a warning but it's currently not possible to set
+# max_tree_depth on a loofah DocumentFragment
+Nokogiri::Gumbo::DEFAULT_MAX_TREE_DEPTH = 2000
+Nokogiri::Gumbo::DEFAULT_MAX_ATTRIBUTES = 2000
