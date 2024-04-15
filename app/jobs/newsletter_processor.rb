b9a7b736933fb639ee200f9bc5dfa2dec8944c55
Ben Ubois <ben@benubois.com>
2024-04-15 15:41:35 +0200

    Add newsletter title if not present.

--- app/jobs/newsletter_processor.rb
+++
@@ -1,8 +0,0 @@
-class NewsletterProcessor
-  include Sidekiq::Worker
-  sidekiq_options queue: :network_default
-
-  def perform(to, url)
-    NewsletterReceiver.new.perform(to, url)
-  end
-end
---
+++ test/jobs/newsletter_saver_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class NewsletterSaverTest < ActiveSupport::TestCase
+  setup do
+    Sidekiq::Worker.clear_all
+  end
+
+  test "Saves to storage" do
+    entry = create_entry(Feed.first)
+    document = "<html><head><title>#{entry.title}</title></head><body>#{entry.content}</body></html>"
+    stub_request(:put, /s3\.amazonaws\.com/)
+      .with(headers: {
+    	  "Content-Encoding"    => "gzip",
+    	  "Content-Type"        => "text/html; charset=utf-8",
+    	  "X-Amz-Acl"           => "public-read",
+    	  "X-Amz-Storage-Class" => "REDUCED_REDUNDANCY"
+      })
+      .with { |request| document == ActiveSupport::Gzip.decompress(request.body)}
+
+    saver = NewsletterSaver.new.perform(entry.id)
+  end
+end
