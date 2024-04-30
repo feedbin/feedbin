fa95995d5fdeadd97d847ed26821b175b2db3f7d
Ben Ubois <ben@benubois.com>
2024-04-30 11:33:52 +0200

    Misc updates

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,21 +7,21 @@ include Clockwork
 # Use locks so multiple clock processes do not schedule dupes
 every(10.seconds, "clockwork.very_frequent") do
   if RedisLock.acquire("clockwork:send_stats:v3", 8)
-    SendStats.perform_async if Rails.env.production?
+    SendStats.perform_async
   end
 
   if RedisLock.acquire("clockwork:cache_entry_views", 8)
-    CacheEntryViews.perform_async(nil, true) if Rails.env.production?
+    CacheEntryViews.perform_async(nil, true)
   end
 
   if RedisLock.acquire("clockwork:downloader_migration", 8)
-    FeedCrawler::PersistCrawlData.perform_async if Rails.env.production?
+    FeedCrawler::PersistCrawlData.perform_async
   end
 end
 
 every(1.minutes, "clockwork.frequent") do
   if RedisLock.acquire("clockwork:feed:refresher:scheduler:v2")
-    FeedCrawler::ScheduleAll.perform_async if Rails.env.production?
+    FeedCrawler::ScheduleAll.perform_async
   end
 
   if RedisLock.acquire("clockwork:harvest:embed:data")
@@ -31,19 +31,17 @@ end
 
 every(1.day, "clockwork.daily", at: "7:00", tz: "UTC") do
   if RedisLock.acquire("clockwork:delete_entries:v2")
-    EntryDeleterScheduler.perform_async if Rails.env.production?
+    EntryDeleterScheduler.perform_async
   end
 
   if RedisLock.acquire("clockwork:trial_expiration:v2")
-    TrialExpiration.perform_async if Rails.env.production?
+    TrialExpiration.perform_async
   end
-end
 
   if RedisLock.acquire("clockwork:web_sub_maintenance")
     WebSub::Maintenance.perform_async
   end
 end
-
 
 every(1.week, "clockwork.weekly", at: "Sunday 16:00", tz: "UTC") do
   if RedisLock.acquire("clockwork:feed_fixer_scheduler")
