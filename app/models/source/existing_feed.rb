3124d631c71745a6d3900ced8bfbf86054da3dad
Ben Ubois <ben@benubois.com>
2024-11-14 14:45:05 +0100

    Re-parse existing feeds.

--- app/models/source/existing_feed.rb
+++ app/models/source/existing_feed.rb
@@ -1,6 +1,7 @@
 class Source::ExistingFeed < Source
   def find
     if feed = Feed.xml.where(feed_url: response.url).take
+      response.parse # check if it's actually a feed
       feeds.push(feed)
     end
   end
