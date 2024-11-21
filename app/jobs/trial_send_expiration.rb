618bd7456a852fa0a1a4e2aaf7e779b4339d857b
Ben Ubois <ben@benubois.com>
2024-11-21 11:34:41 +0100

    Skip expiration notice if there's no activity.

--- app/jobs/trial_send_expiration.rb
+++ app/jobs/trial_send_expiration.rb
@@ -4,7 +4,7 @@ class TrialSendExpiration
 
   def perform(user_id)
     user = User.where(id: user_id).first
-    if user.present? && user.plan.stripe_id == "trial"
+    if user.present? && user.plan.stripe_id == "trial" && user.subscriptions.exists?
       UserMailer.trial_expiration(user_id).deliver
     end
   end
