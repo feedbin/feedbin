14f15b268bf1ec13b7beef0ef7aa30632f66853d
Ben Ubois <ben@benubois.com>
2025-02-08 21:57:58 +0100

    Fix method name

--- app/views/components/common/error_message.rb
+++ app/views/components/common/error_message.rb
@@ -1,5 +1,5 @@
 class Common::ErrorMessage < Phlex::HTML
-  def template
+  def view_template
     p(class: "flex items-center p-2 bg-red-200 gap-2 rounded-lg mb-4") do
       render SvgComponent.new "icon-error-message-small", class: "fill-red-600"
       span(class: "text-[rbg(var(--day-color-600))]") { yield }
