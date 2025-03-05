5eeb5ae798a8db064f196a67082c4340faccadec
Ben Ubois <ben@benubois.com>
2025-03-05 19:01:07 +0100

    Dialog padding

--- app/views/components/dialog/template.rb
+++ app/views/components/dialog/template.rb
@@ -58,15 +58,15 @@ module Dialog
       def view_template
         div class: "flex flex-col max-h-dvh min-h-dvh sm:overflow-hidden sm:min-h-0 sm:max-h-[calc(100vh-128px)]" do
           div class: "shrink-0 h-[env(safe-area-inset-top)]"
-          div class: "p-4 native:pt-[5px] text-base flex items-baseline shrink-0 relative border-b border-transparent group-data-[dialog-header-border-value=true]:border-200" do
-            button type: "button", class: "absolute shrink-0 left-0 inset-y-0 px-4 text-600", data: stimulus_item(actions: {click: :close}, for: STIMULUS_CONTROLLER) do
+          div class: "p-4 sm:px-6 native:pt-[5px] text-base flex items-baseline shrink-0 relative border-b border-transparent group-data-[dialog-header-border-value=true]:border-200" do
+            button type: "button", class: "absolute shrink-0 left-0 inset-y-0 px-4 sm:px-6 text-600", data: stimulus_item(actions: {click: :close}, for: STIMULUS_CONTROLLER) do
               render SvgComponent.new "icon-close", class: "relative native:top-[-6px] fill-600", title: "Close"
             end
             h2 class: "text-700 grow font-bold m-0 truncate text-center", &@title
           end
 
-          div data: stimulus_item(target: :content, actions: {scroll: :check_scroll}, data: {template: "body"}, for: STIMULUS_CONTROLLER), class: "p-4 overflow-y-scroll sm:overscroll-y-contain grow relative transition-[height] duration-200 ease-out", &@body
-          div data: {template: "footer"}, class: "py-2 sm:py-4 px-4 shrink-0 relative text-right transition-all border-t border-transparent group-data-[dialog-footer-border-value=true]:border-200 #{footer? ? "" : "tw-hidden"}", &@footer
+          div data: stimulus_item(target: :content, actions: {scroll: :check_scroll}, data: {template: "body"}, for: STIMULUS_CONTROLLER), class: "p-4 sm:px-6 overflow-y-scroll sm:overscroll-y-contain grow relative transition-[height] duration-200 ease-out", &@body
+          div data: {template: "footer"}, class: "py-2 px-4 sm:p-6 shrink-0 relative text-right transition-all border-t border-transparent group-data-[dialog-footer-border-value=true]:border-200 #{footer? ? "" : "tw-hidden"}", &@footer
           div data: stimulus_item(target: :footer_spacer, for: STIMULUS_CONTROLLER), class: "shrink-0 transition-all h-[max(var(--visual-viewport-offset),env(safe-area-inset-bottom))] group-data-[dialog-footer-value=false]:tw-hidden #{footer? ? "" : "tw-hidden"}"
         end
       end
