685f286476bf5025dbb022f795873e4d4dcafb68
Ben Ubois <ben@benubois.com>
2025-06-05 16:03:34 +0200

    Count stats

--- app/jobs/feed_crawler/schedule_batch.rb
+++ app/jobs/feed_crawler/schedule_batch.rb
@@ -8,6 +8,7 @@ module FeedCrawler
     attr_accessor :force_refresh
 
     def perform(batch, priority_refresh)
+      host_counts = {}
       feed_ids = build_ids(batch).shuffle
       count = priority_refresh ? 1 : 0
 
@@ -26,6 +27,8 @@ module FeedCrawler
 
       jobs = (subscriptions + standalone).filter_map do |feed|
         if feed.crawl_data.ok?(feed.feed_url)
+          host_counts[feed.host] ||= 0
+          host_counts[feed.host] += 1
           [feed.id, feed.feed_url, feed.subscriptions_count, feed.crawl_data.to_h]
         end
       end
@@ -35,7 +38,18 @@ module FeedCrawler
           "args"  => jobs.shuffle,
           "class" => Downloader
         )
+        log_counts(host_counts)
       end
+    end
+
+    def log_counts(counts)
+      result = counts
+        .select { _2 > 1 }
+        .sort_by { -(_2) }
+        .first(100)
+        .map { "#{_1}=#{_2}" }
+        .join(" ")
+      Sidekiq.logger.info "ScheduleBatch counts #{result}"
     end
   end
 end
