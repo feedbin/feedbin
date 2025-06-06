209ca26a18d49b76c0aa8d94c8a9f694117ebc3d
Ben Ubois <ben@benubois.com>
2025-06-06 12:08:09 +0200

    Combine crawl schedule jobs.

--- app/jobs/feed_crawler/schedule_all.rb
+++ app/jobs/feed_crawler/schedule_all.rb
@@ -5,34 +5,54 @@ module FeedCrawler
     sidekiq_options queue: :utility_critical
 
     COUNT_KEY = "feed_refresher_scheduler:count".freeze
+    LAST_REFRESH_KEY = "feed_refresher_scheduler:last_refresh".freeze
 
     def perform
       queues = [Downloader, Parser, Receiver]
-      if queues.all? {|queue| queue_empty?(queue.get_sidekiq_options["queue"]) }
-        refresh_feeds
+      unless queues.all? { queue_empty?(it.get_sidekiq_options["queue"]) }
+        Sidekiq.logger.info "skipping, crawl still processing"
+        return
       end
+
+      unless last_refresh.before?(15.minutes.ago)
+        Sidekiq.logger.info "skipping, last crawl too recent"
+        return
+      end
+
+      refresh_feeds
     end
 
     def refresh_feeds
-      feed = Feed.last
-      if feed
-        jobs = job_args(feed.id, 1, priority?)
-        Sidekiq::Client.push_bulk(
-          "args" => jobs,
-          "class" => ScheduleBatch
-        )
-        increment
-        report
+      subscribed_feed_ids = Subscription.where(active: true).pluck("DISTINCT feed_id")
+      standalone_feed_ids = Feed.where.not(standalone_request_at: nil).pluck(:id)
+      feed_ids = (subscribed_feed_ids + standalone_feed_ids).uniq.shuffle
+
+      feed_ids.each_slice(5_000) do |ids|
+        jobs = Feed.xml.where(id: ids).filter_map do |feed|
+          if feed.crawl_data.ok?(feed.feed_url)
+            [feed.id, feed.feed_url, feed.subscriptions_count, feed.crawl_data.to_h]
+          end
+        end
+
+        Sidekiq::Client.push_bulk("args" => jobs, "class" => Downloader) unless jobs.empty?
       end
+
+      increment
+      report
     end
 
     def priority?
       @priority ||= count % 2 == 0
     end
 
+    def last_refresh
+      last_refresh = Time.at(Sidekiq.redis { it.get(LAST_REFRESH_KEY) }.to_i)
+    end
+
     def increment
       Librato.increment "refresh_feeds"
       Sidekiq.redis { _1.incr(COUNT_KEY) }
+      Sidekiq.redis { _1.set(LAST_REFRESH_KEY, Time.now.to_i) }
     end
 
     def report
--- test/jobs/feed_crawler/schedule_all_test.rb
+++ test/jobs/feed_crawler/schedule_all_test.rb
@@ -1,25 +1,86 @@
 require "test_helper"
 
 module FeedCrawler
-  class ScheduleAllTest < ActiveSupport::TestCase
-    test "should enqueue FeedRefresher" do
-      flush_redis
-      assert_difference -> { ScheduleBatch.jobs.size }, +1 do
-        job = perform
-        assert job.priority?
-      end
+  class ScheduleTest < ActiveSupport::TestCase
 
-      assert_not perform.priority?
+    setup do
+      flush_redis
+      @user = users(:ben)
+      @feed = @user.feeds.first
+      Feed.all.each do |feed|
+        Feed.reset_counters(feed.id, :subscriptions)
+      end
     end
 
-    private
-
-    def perform
-      ScheduleAll.new.tap do |job|
-        def job.job_args(*args)
-          [[1]]
+    test "should enqueue feed_downloader jobs" do
+      assert_difference -> { Downloader.jobs.size }, Feed.count do
+        Schedule.new.perform
+        Downloader.jobs.each do |job|
+          feed = Feed.find(job["args"][0])
+          assert_equal(feed.feed_url, job["args"][1])
+          assert_equal(feed.subscriptions_count, job["args"][2])
+          assert_equal(feed.crawl_data.to_h, job["args"][3].symbolize_keys)
         end
-        job.perform
+      end
+    end
+
+    test "skips enqueue for throttled feed" do
+      hosts = Feed.all.pluck(:host)
+      ENV["THROTTLED_HOSTS"] = hosts.map {"#{it}=1"}.join(" ")
+
+      Feed.all.each do |feed|
+        feed.crawl_data.log_download
+        feed.save!
+      end
+
+      assert_no_difference -> { Downloader.jobs.size } do
+        Schedule.new.perform
+      end
+
+      travel (Throttle::TIMEOUT * 2).seconds do
+        assert_difference -> { Downloader.jobs.size }, Feed.count do
+          Schedule.new.perform
+        end
+      end
+    end
+
+    test "skips enqueue for feeds with errors" do
+      Feed.all.each do |feed|
+        feed.crawl_data.download_error(Exception.new)
+        feed.save!
+      end
+
+
+      assert_no_difference -> { Downloader.jobs.size } do
+        Schedule.new.perform
+      end
+
+      Feed.all.each do |feed|
+        feed.crawl_data.clear!
+        feed.save!
+      end
+      flush_redis
+
+      assert_difference -> { Downloader.jobs.size }, +Feed.count do
+        Schedule.new.perform
+      end
+
+    end
+
+    test "enqueue for feeds with errors after backoff" do
+      Feed.all.each do |feed|
+        feed.crawl_data.download_error(Exception.new)
+        feed.save!
+      end
+
+      assert_no_difference -> { Downloader.jobs.size } do
+        Schedule.new.perform
+      end
+
+      travel 2.hours do
+        assert_difference -> { Downloader.jobs.size }, Feed.count do
+          Schedule.new.perform
+        end
       end
     end
   end
