d69fee6c3f1cee6ec62d41e5e6d4f786a66b66c7
Ben Ubois <ben@benubois.com>
2025-06-11 13:43:57 +0200

    Format palette updates.

--- config/initializers/themes.rb
+++ config/initializers/themes.rb
@@ -1,9 +1,9 @@
 Rails.application.reloader.to_prepare do
   Feedbin::Application.config.themes = [
+    Theme.new("Auto", "auto"),
     Theme.new("Day", "day"),
     Theme.new("Sunset", "sunset"),
     Theme.new("Dusk", "dusk"),
-    Theme.new("Night", "midnight"),
-    Theme.new("Auto", "auto")
+    Theme.new("Night", "midnight")
   ]
 end
