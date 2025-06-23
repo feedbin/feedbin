271d917efadc1971e51f7d113d48188974fc986f
Krzysztof Dąbrowski <krzysdabro@live.com>
2025-06-23 20:24:08 +0200

    Fix S3 name when using a single storage bucket

--- app/jobs/image_deleter.rb
+++ app/jobs/image_deleter.rb
@@ -1,11 +1,13 @@
 class ImageDeleter
   include Sidekiq::Worker
 
+  BUCKET = ENV["AWS_S3_BUCKET_IMAGES"] || ENV["AWS_S3_BUCKET"]
+
   def perform(image_urls)
     client = Fog::Storage.new(STORAGE)
     paths = extract_paths(image_urls)
     paths.each_slice(999) do |slice|
-      client.delete_multiple_objects(ENV["AWS_S3_BUCKET_IMAGES"], slice, {quiet: true})
+      client.delete_multiple_objects(BUCKET, slice, {quiet: true})
     end
     Librato.increment "entry_image.delete", by: image_urls.length
   end
