135a53644a8b5c80fed35cfc32fb556683568576
Ben Ubois <ben@benubois.com>
2025-07-13 15:19:46 +0200

    debug.

--- app/jobs/save_page_from_extension.rb
+++ app/jobs/save_page_from_extension.rb
@@ -4,7 +4,8 @@ class SavePageFromExtension
   sidekiq_options queue: local_queue("parse_critical"), retry: false
 
   def perform(user_id, url, title, path)
+    Sidekiq.logger.info "Saving page path=#{path}"
     SavePage.new.perform(user_id, url, title, path)
-    File.unlink(path) rescue Errno::ENOENT
+    # File.unlink(path) rescue Errno::ENOENT
   end
 end
