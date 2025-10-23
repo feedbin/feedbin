f249363cea40b31e621e1bac6dc68f8a3fac9e7d
Ben Ubois <ben@benubois.com>
2025-10-23 20:56:32 +0200

    Turn off unused stats.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -4,18 +4,18 @@ ActiveSupport::Notifications.subscribe("process_action.action_controller") do |n
     action = payload[:params]["action"]
     if finish && start
       time = (finish - start) * 1000
-      Librato.timing "controller.#{controller}.#{action}.time", time, percentile: [95]
+      Librato.timing "controller.#{controller}.#{action}.time", time
       if payload[:params].present? && payload[:params]["subdomain"].present? && payload[:params]["subdomain"] == "api"
-        Librato.timing "response_time.api", time, source: Socket.gethostname, percentile: [95]
+        Librato.timing "response_time.api", time, source: Socket.gethostname
       else
-        Librato.timing "response_time.web", time, source: Socket.gethostname, percentile: [95]
+        Librato.timing "response_time.web", time, source: Socket.gethostname
       end
     end
     if payload[:db_runtime]
-      Librato.timing "controller.#{controller}.#{action}.time.db", payload[:db_runtime], percentile: [95]
+      Librato.timing "controller.#{controller}.#{action}.time.db", payload[:db_runtime]
     end
     if payload[:view_runtime]
-      Librato.timing "controller.#{controller}.#{action}.time.view", payload[:view_runtime], percentile: [95], source: Socket.gethostname
+      Librato.timing "controller.#{controller}.#{action}.time.view", payload[:view_runtime], source: Socket.gethostname
     end
   end
 end
