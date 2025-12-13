41657b4a6b2b693f5934259085454c17c1321c78
Ben Ubois <ben@benubois.com>
2025-12-13 13:07:16 -0800

    Misc updates.

--- app/javascript/controllers/onboarding/main_controller.js
+++ app/javascript/controllers/onboarding/main_controller.js
@@ -2,11 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="onboarding--main"
 export default class extends Controller {
+  static targets = ["panel", "viewport", "scrollTrack"]
+
   static values = {
     step: String
   }
 
   connect() {
     console.log("onboarding--main connected");
+  }
+
+  selectPanel(event) {
+    const panelName = event.params.panel
+    this.stepValue = panelName
+
+    const panelIndex = this.panelTargets.findIndex(element => element.dataset.panel === panelName);
+    const panelElement = this.panelTargets[panelIndex]
+    const viewport = this.viewportTarget
+
+    const panelElementRect = panelElement.getBoundingClientRect()
+    const viewportRect = viewport.getBoundingClientRect()
+    const offset = (panelElementRect.left - viewportRect.left) * -1
+
+    this.scrollTrackTarget.style.transform = `translateX(${offset}px)`;
+
+
+    console.log(offset);
+
+    console.log(panelElement);
   }
 }
--- app/views/onboarding/show_view.rb
+++ app/views/onboarding/show_view.rb
@@ -80,38 +80,36 @@ module Onboarding
     end
 
     def view_template
-      div data: stimulus(controller: STIMULUS_CONTROLLER, values: {step: :welcome}), class: "group border rounded-xl w-[456px] h-[700px] flex flex-col" do
-        div class: "p-4 sm:px-6 native:pt-[5px] flex items-baseline shrink-0 relative border-b" do
-          button class: "shrink-0" do
-            "Back"
+      div data: stimulus(controller: STIMULUS_CONTROLLER, values: {step: :welcome}), class: "group h-full w-full flex flex-center" do
+        div class: "border rounded-xl w-[456px] h-[700px] flex flex-col" do
+          div class: "p-4 sm:px-6 native:pt-[5px] flex items-baseline shrink-0 relative border-b" do
+            button class: "shrink-0" do
+              "Back"
+            end
+            div class: "text-700 grow font-bold m-0 truncate text-center" do
+              "Get Started"
+            end
+            button type: "button", class: "absolute shrink-0 right-0 inset-y-0 px-4 sm:px-6 text-600", data: stimulus_item(actions: {click: :close}, for: STIMULUS_CONTROLLER) do
+              Icon("icon-close", class: "relative native:top-[-6px] fill-600", title: "Close")
+            end
           end
-          div class: "text-700 grow font-bold m-0 truncate text-center" do
-            "Get Started"
-          end
-          button type: "button", class: "absolute shrink-0 right-0 inset-y-0 px-4 sm:px-6 text-600", data: stimulus_item(actions: {click: :close}, for: STIMULUS_CONTROLLER) do
-            Icon("icon-close", class: "relative native:top-[-6px] fill-600", title: "Close")
-          end
-        end
-        div class: "relative grow min-h-0 w-full h-full overflow-hidden focus:outline-none" do
-          div class: "flex h-full transition-transform duration-500 ease-[cubic-bezier(0.22,0.61,0.36,1)] will-change-transform" do
-            [:welcome, :add, :import, :extension].each do |page|
-              div class: "flex-none w-full min-w-full h-full" do
-                div class: "h-full box-border p-6 overflow-y-auto" do
-                  send(page)
-                end
+          div data: stimulus_item(target: :viewport, for: STIMULUS_CONTROLLER), class: "relative grow min-h-0 w-full h-full overflow-hidden focus:outline-none" do
+            div data: stimulus_item(target: :scroll_track, for: STIMULUS_CONTROLLER), class: "flex h-full transition-transform duration-300 will-change-transform ease-[cubic-bezier(0.22,0.61,0.36,1)]" do
+              [:welcome, :add, :import, :extension].each do |panel|
+                send(panel)
               end
             end
           end
-        end
-        div class: "px-4 sm:px-6 h-[77px] flex items-center shrink-0 relative border-t" do
-          div class: "tw-hidden group-data-[onboarding--main-step-value=welcome]:flex grow flex-center" do
-            button class: "text-blue-600" do
-              "Skip"
+          div class: "px-4 sm:px-6 h-[77px] flex items-center shrink-0 relative border-t" do
+            div class: "tw-hidden group-data-[onboarding--main-step-value=welcome]:flex grow flex-center" do
+              button class: "text-blue-600" do
+                "Skip"
+              end
             end
-          end
-          div class: "block group-data-[onboarding--main-step-value=welcome]:tw-hidden" do
-            button class: "ml-auto button" do
-              "Continue"
+            div class: "block group-data-[onboarding--main-step-value=welcome]:tw-hidden" do
+              button class: "ml-auto button" do
+                "Continue"
+              end
             end
           end
         end
@@ -119,107 +117,118 @@ module Onboarding
     end
 
     def welcome
-      div class: "flex flex-center flex-col h-full" do
-        Icon("logo-dynamic", class: "w-[54px] h-auto mb-4")
-        div class: "text-2xl text-700 font-bold mb-10" do
-          "Welcome to Feedbin"
-        end
+      render PanelView.new(panel: :welcome) do
+        div class: "flex flex-center flex-col h-full" do
+          Icon("logo-dynamic", class: "w-[54px] h-auto mb-4")
+          div class: "text-2xl text-700 font-bold mb-10" do
+            "Welcome to Feedbin"
+          end
 
-        div class: "flex flex-col gap-4 w-full" do
-          big_button(
-            title: "Import Subscriptions",
-            subtitle: "Import an OPML file from another service",
-            icon: "menu-icon-import-export"
-          )
-          big_button(
-            title: "Browse Feeds",
-            subtitle: "Choose feeds to add ",
-            icon: "icon-search"
-          )
+          div class: "flex flex-col gap-4 w-full" do
+            big_button(
+              title: "Import Subscriptions",
+              subtitle: "Import an OPML file from another service",
+              icon: "menu-icon-import-export",
+              panel: :import
+            )
+            big_button(
+              title: "Browse Feeds",
+              subtitle: "Choose feeds to add ",
+              icon: "icon-search",
+              panel: :add
+            )
+          end
         end
       end
     end
-
     def add
       controller = :onboarding__subscriptions
-      div data: stimulus(controller: controller, values: {selected_count: 0}), class: "group flex flex-col h-full overflow-y-auto min-h-0 hide-scrollbar" do
-        div class: "flex gap-4 justify-between items-baseline w-full pb-4 shrink-0 bg-base sticky top-0" do
-          div class: "text-xl font-bold" do
-            "Add Content"
+      render PanelView.new(panel: :add, padding: false, attributes: {class: "tw-hidden group-data-[onboarding--main-step-value=add]:block"}) do
+        div data: stimulus(controller: controller, values: {selected_count: 0}), class: "group flex flex-col h-full min-h-0" do
+          div class: "flex gap-4 justify-between items-baseline w-full p-6 pb-4 shrink-0 bg-base sticky top-0" do
+            div class: "text-xl font-bold" do
+              "Add Content"
+            end
+            button data: stimulus_item(actions: {click: :clear_all} , for: controller), class: "text-500 group-data-[onboarding--subscriptions-selected-count-value=0]:tw-hidden" do
+              plain "Clear All ("
+              span data: stimulus_item(target: :count, for: controller)
+              plain ")"
+            end
           end
-          button data: stimulus_item(actions: {click: :clear_all} , for: controller), class: "text-500 group-data-[onboarding--subscriptions-selected-count-value=0]:tw-hidden" do
-            plain "Clear All ("
-            span data: stimulus_item(target: :count, for: controller)
-            plain ")"
-          end
-        end
-        div class: "grid grid-cols-2 sm:grid-cols-3 gap-4 min-h-0 p-[2px] flex-1" do
-          SITES.shuffle.each do
-            tile(title: it[:title], subtitle: it[:host], image: it[:image], controller: controller)
+          div class: "min-h-0 flex-1 px-6" do
+            div class: "grid grid-cols-2 sm:grid-cols-3 gap-4 pb-6" do
+              SITES.shuffle.each do
+                tile(title: it[:title], subtitle: it[:host], image: it[:image], controller: controller)
+              end
+            end
           end
         end
       end
     end
 
     def extension
-      div class: "flex flex-col gap-4" do
+      render PanelView.new(panel: :extension) do
+        div class: "flex flex-col gap-4" do
 
-        div class: "text-xl font-bold" do
-          "Get the Extension"
-        end
-
-        img src: asset_path("extension-promo"), class: "rounded"
-
-        div class: "font-bold" do
-          "Subscribe to feeds, newsletters, and save pages to Feedbin"
-        end
-        ul class: "pl-[40px] list-disc [&_li]:[list-style:disc] flex flex-col gap-2" do
-          li do
-            "Subscribe to feeds in Feedbin while browsing the web"
+          div class: "text-xl font-bold" do
+            "Get the Extension"
           end
-          li do
-            "Save the page you‘re on to read later"
-          end
-          li do
-            "Create email addresses for newsletter subscriptions"
-          end
-        end
-        button class: "button button-secondary" do
-          "Get for Safari"
-        end
 
+          img src: asset_path("extension-promo"), class: "rounded"
+
+          div class: "font-bold" do
+            "Subscribe to feeds, newsletters, and save pages to Feedbin"
+          end
+          ul class: "pl-[40px] list-disc [&_li]:[list-style:disc] flex flex-col gap-2" do
+            li do
+              "Subscribe to feeds in Feedbin while browsing the web"
+            end
+            li do
+              "Save the page you‘re on to read later"
+            end
+            li do
+              "Create email addresses for newsletter subscriptions"
+            end
+          end
+          button class: "button button-secondary" do
+            "Get for Safari"
+          end
+
+        end
       end
     end
 
     def import
       controller = :upload
-      div class: "w-full h-full overflow-y-auto hide-scrollbar", data: {behavior: "onboarding_import"} do
-        div class: "w-full h-full group", data: stimulus(controller: controller, values: {dragging: false, dropped: false, error: false}, actions: { "upload:serverError@window" => "serverError"}) do
-          input(
+      render PanelView.new(panel: :import, attributes: {class: "tw-hidden group-data-[onboarding--main-step-value=import]:block"}) do
+        div class: "w-full h-full", data: {behavior: "onboarding_import"} do
+          div class: "w-full h-full group", data: stimulus(controller: controller, values: {dragging: false, dropped: false, error: false}, actions: { "upload:serverError@window" => "serverError"}) do
+            input(
             type: "file",
             accept: ".opml,.xml",
             class: "hidden",
-            name: "import[upload]",
-            data: stimulus_item(target: :file_input, actions: {change: :file_selected}, for: controller)
-          )
-          form_with class: "w-full h-full group", model: Import.new, url: helpers.onboarding_imports_path, method: :post, data: stimulus_item(target: :form, data: {remote: true}, for: controller) do |form|
-            form.hidden_field :filename, data: stimulus_item(target: :filename_field, for: controller)
-            form.hidden_field :xml, data: stimulus_item(target: :xml_field, for: controller)
-            div data: stimulus_item(target: :dropzone, actions: {dragover: :drag_over, dragleave: :drag_leave, drop: :drop, dragstart: :drag_start }, for: controller), class: "w-full h-full border rounded-xl border-dashed flex flex-center transition-colors group-data-[upload-dragging-value=true]:border-blue-700 group-data-[upload-dragging-value=true]:bg-[rgb(var(--color-blue-400)/0.1)] group-data-[upload-error-value=true]:border-red-600 group-data-[upload-error-value=true]:bg-[rgb(var(--color-red-600)/0.1)]" do
-              div class: "flex flex-col gap-4 flex-center group-data-[upload-error-value=true]:tw-hidden" do
-                Icon("icon-cloud", class: "fill-600")
-                div class: "" do
-                  "Drag & Drop OPML Here"
+              name: "import[upload]",
+              data: stimulus_item(target: :file_input, actions: {change: :file_selected}, for: controller)
+            )
+            form_with class: "w-full h-full group", model: Import.new, url: helpers.onboarding_imports_path, method: :post, data: stimulus_item(target: :form, data: {remote: true}, for: controller) do |form|
+              form.hidden_field :filename, data: stimulus_item(target: :filename_field, for: controller)
+              form.hidden_field :xml, data: stimulus_item(target: :xml_field, for: controller)
+              div data: stimulus_item(target: :dropzone, actions: {dragover: :drag_over, dragleave: :drag_leave, drop: :drop, dragstart: :drag_start }, for: controller), class: "w-full h-full border rounded-xl border-dashed flex flex-center transition-colors group-data-[upload-dragging-value=true]:border-blue-700 group-data-[upload-dragging-value=true]:bg-[rgb(var(--color-blue-400)/0.1)] group-data-[upload-error-value=true]:border-red-600 group-data-[upload-error-value=true]:bg-[rgb(var(--color-red-600)/0.1)]" do
+                div class: "flex flex-col gap-4 flex-center group-data-[upload-error-value=true]:tw-hidden" do
+                  Icon("icon-cloud", class: "fill-600")
+                  div class: "" do
+                    "Drag & Drop OPML Here"
+                  end
+                  div class: "font-bold" do
+                    "OR"
+                  end
+                  button data: stimulus_item(actions: {click: :choose_file}, for: controller), type: "button", class: "button button-secondary" do
+                    "Choose File"
+                  end
                 end
-                div class: "font-bold" do
-                  "OR"
+                div class: "flex-col gap-4 tw-hidden flex-center group-data-[upload-error-value=true]:flex" do
+                  div data: stimulus_item(target: :error_message, for: controller)
                 end
-                button data: stimulus_item(actions: {click: :choose_file}, for: controller), type: "button", class: "button button-secondary" do
-                  "Choose File"
-                end
-              end
-              div class: "flex-col gap-4 tw-hidden flex-center group-data-[upload-error-value=true]:flex" do
-                div data: stimulus_item(target: :error_message, for: controller)
               end
             end
           end
@@ -240,8 +249,8 @@ module Onboarding
       end
     end
 
-    def big_button(title:, subtitle:, icon:)
-      button class: "border rounded-xl flex items-center gap-4 p-4 grow text-left" do
+    def big_button(title:, subtitle:, icon:, panel:)
+      button data: stimulus_item(actions: {click: :selectPanel}, params: {panel: panel}, for: STIMULUS_CONTROLLER), class: "border rounded-xl flex items-center gap-4 p-4 grow text-left" do
         div class: "w-[30px] flex flex-center shrink-0" do
           Icon(icon, class: "fill-500")
         end
@@ -260,6 +269,23 @@ module Onboarding
         end
       end
     end
+
+    class PanelView < ApplicationComponent
+      def initialize(panel:, padding: true, attributes: {})
+        @panel = panel
+        @padding = padding
+        @attributes = attributes
+      end
+
+      def view_template
+        div data: stimulus_item(target: :panel, for: STIMULUS_CONTROLLER, data: {panel: @panel}), **mix({ class: "flex-none w-full min-w-full h-full" }, @attributes) do
+          div class: "h-full box-border overflow-y-auto #{@padding ? "p-6" : ""}" do
+            yield
+          end
+        end
+      end
+    end
+
 
   end
 end
