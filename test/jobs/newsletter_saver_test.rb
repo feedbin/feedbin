a82a02ef60b09f20264d14b7c080bec0de193017
Ben Ubois <ben@benubois.com>
2026-03-05 10:49:21 -0800

    Test fixes

--- test/jobs/newsletter_saver_test.rb
+++ test/jobs/newsletter_saver_test.rb
@@ -8,7 +8,7 @@ class NewsletterSaverTest < ActiveSupport::TestCase
   test "Saves to storage" do
     entry = create_entry(Feed.first)
     document = "<html><head><title>#{entry.title}</title></head><body>#{entry.content}</body></html>"
-    stub_request(:put, /s3\.amazonaws\.com/)
+    request = stub_request(:put, /s3\.amazonaws\.com/)
       .with(headers: {
         "Content-Encoding"    => "gzip",
         "Content-Type"        => "text/html; charset=utf-8",
@@ -17,16 +17,18 @@ class NewsletterSaverTest < ActiveSupport::TestCase
       })
       .with { |request| document == ActiveSupport::Gzip.decompress(request.body)}
 
-    saver = NewsletterSaver.new.perform(entry.id)
+    NewsletterSaver.new.perform(entry.id)
+    assert_requested request
   end
 
   test "Saves text email to storage" do
     entry = create_entry(Feed.first)
     entry.update(data: {format: "text"})
     document = "<title>#{entry.title}</title>"
-    stub_request(:put, /s3\.amazonaws\.com/)
+    request = stub_request(:put, /s3\.amazonaws\.com/)
       .with { |request| ActiveSupport::Gzip.decompress(request.body).include?(document) }
 
-    saver = NewsletterSaver.new.perform(entry.id)
+    NewsletterSaver.new.perform(entry.id)
+    assert_requested request
   end
 end
