cd308a300a9a3519ef4e854162d136f55b846e75
Ben Ubois <ben@benubois.com>
2026-07-09 10:46:27 +0200

    Include parallel test worker number in search index names

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,6 @@
 module Search
   def self.index_name(base_name)
-    Rails.env.test? ? "test-#{base_name}" : base_name
+    return base_name unless Rails.env.test?
+    ["test", ENV["TEST_WORKER"], base_name].compact.join("-")
   end
 end
--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -2,7 +2,15 @@ require "test_helper"
 
 class SearchTest < ActiveSupport::TestCase
   test "prefixes the base name in the test environment" do
-    assert_equal "test-entries", Search.index_name("entries")
+    with_test_worker(nil) do
+      assert_equal "test-entries", Search.index_name("entries")
+    end
+  end
+
+  test "includes the parallel worker number when TEST_WORKER is set" do
+    with_test_worker("3") do
+      assert_equal "test-3-entries", Search.index_name("entries")
+    end
   end
 
   test "returns the base name unchanged outside the test environment" do
@@ -12,8 +20,19 @@ class SearchTest < ActiveSupport::TestCase
   end
 
   test "the search alias config is namespaced for the test environment" do
-    assert_equal "test-entries-01", $search[:config][:aliases][:entries]
-    assert_equal "test-actions-01", $search[:config][:aliases][:actions]
-    assert_equal "test-feeds-01", $search[:config][:aliases][:feeds]
+    prefix = ["test", ENV["TEST_WORKER"]].compact.join("-")
+    assert_equal "#{prefix}-entries-01", $search[:config][:aliases][:entries]
+    assert_equal "#{prefix}-actions-01", $search[:config][:aliases][:actions]
+    assert_equal "#{prefix}-feeds-01", $search[:config][:aliases][:feeds]
+  end
+
+  private
+
+  def with_test_worker(number)
+    original = ENV["TEST_WORKER"]
+    ENV["TEST_WORKER"] = number
+    yield
+  ensure
+    original.nil? ? ENV.delete("TEST_WORKER") : ENV["TEST_WORKER"] = original
   end
 end
